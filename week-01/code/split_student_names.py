bff37af4605d4b35873d83ad1814a1851f42904c
rakitaj <joshuarakita@gmail.com>
2013-10-01 00:53:13 -0700

    Adding python script to generate student names and the file of those names.

---
+++ week-01/code/split_student_names.py
@@ -0,0 +1,14 @@
+studentNames = list()
+fr = open('C:\Users\Josh\PYTHON100\courseenrollees 9-30-13 - Sheet1.csv')
+lines = fr.readlines()
+fr.close()
+#print(type(lines))
+#print(len(lines))
+for line in lines:
+	segments = line.split(',')
+	studentNames.append(segments[0] + ", " + segments[1])
+# Python is smart enough to not keep on appending to this file when run multiple times
+fw = open('C:\Users\Josh\PYTHON100\IntroToPython\week-01\students.txt', 'w')
+for name in studentNames:
+	fw.write(str(name) + '\n')
+fw.close()
