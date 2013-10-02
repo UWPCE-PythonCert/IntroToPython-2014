8ecc8164515184903b5d9506090675bc473b88db
Chris Barker <Chris.Barker@noaa.gov>
2013-10-01 20:28:24 -0700

    added languages to students file

--- week-01/code/split_student_names.py
+++ week-01/code/split_student_names.py
@@ -1,14 +1,22 @@
-studentNames = list()
+#!/usr/bin/env python
+
 fr = open('C:\Users\Josh\PYTHON100\courseenrollees 9-30-13 - Sheet1.csv')
 lines = fr.readlines()[1:]
 fr.close()
+
 #print(type(lines))
 #print(len(lines))
+
+studentNames = []
 for line in lines:
 	segments = line.split(',')
 	studentNames.append(segments[0] + ", " + segments[1])
+
 # Python is smart enough to not keep on appending to this file when run multiple times
+# (use the 'a' flag if you DO want to append)
+
 fw = open('C:\Users\Josh\PYTHON100\IntroToPython\week-01\students.txt', 'w')
+
 for name in studentNames:
-	fw.write(str(name) + '\n')
+	fw.write(name + '\n')
 fw.close()
