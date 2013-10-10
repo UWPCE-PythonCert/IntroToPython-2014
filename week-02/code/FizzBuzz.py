9434d8ca94ee80fff59e7495d71e0131cb89ac81
Chris Barker <Chris.Barker@noaa.gov>
2013-10-10 15:59:05 -0700

    solution to homework week 2, and more on presentation.

--- week-02/code/FizzBuzz.py
+++ week-02/code/FizzBuzz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 
 """
 Fizz Buzz examples -- from most straightforward, to most compact.
@@ -44,8 +44,19 @@ def fizzbuzz4(n):
     """
     the one liner
     """
-    for i in range(1,n+1):
-        print ( "Fizz" * (not (i%3)) + "Buzz" * (not (i%5)) ) or i
+    for i in range(1,n+1): print ( "Fizz" * (not (i%3)) + "Buzz" * (not (i%5)) ) or i
+
+def fizzbuzz_ruby(n):
+    """
+    This is a one-liner version inspired by the Ruby one-liner 
+    found here:
+
+    http://www.commandercoriander.net/blog/2013/02/03/fizzbuzz-in-one-line
+
+    This uses list comprehensions, and slicing, and is, well, pretty darn ugly!
+
+    """
+    for word in [ ("".join(["Fizz",][0:1-i%3]+["Buzz",][0:1-i%5]) or `i`) for i in range(1, n+1)]: print word
 
 
 fizzbuzz1(16)
@@ -55,6 +66,7 @@ print
 fizzbuzz3(16)
 print 
 fizzbuzz4(16)
-
+print 
+fizzbuzz_ruby(16)
 
 
