88a9406e526c0fb1116d92d9ab6c6f120edb04c8
Schuyler Schwafel <saschwafel@gmail.com>
2014-10-07 17:09:23 -0700

    Trying to add 3x3 printing in lieu of n x ng

--- Students/SSchwafel/session01/grid.py
+++ Students/SSchwafel/session01/grid.py
@@ -43,87 +43,39 @@
 #
 #
 
-box_dimensions = input("Please enter how many characters wide you'd like your grid: ")
+user_dimensions = raw_input("Please enter how many characters wide you'd like your 3x3 grid: ")
 
 def print_3x3_grid(box_dimensions):
 
-    box_dimensions = int(box_dimensions)
+    box_dimensions = int(user_dimensions)
 
-# check to see if entered value is odd/even
-# If entry is ODD, there are an even number of boxes, if entry is EVEN, there are an odd number of boxes
+    box_dimensions = box_dimensions
 
-    if box_dimensions % 2 == 0:
-        
-        print '\nbox_dimensions is even'
-        
-    #Figure out how big each box will be 
+    print box_dimensions
 
-        print 
-        print
+    if box_dimensions % 3 == 0 and box_dimensions != 3:
+    
+        box_dimensions = (box_dimensions - 1) / 3
 
-        individual_box_size = box_dimensions/2
-
-        print individual_box_size
-
-        print 
-        print
-
-        #this line figures out how many -- to print ( and subtracts +)
-
-        dashes_in_boxes = individual_box_size - 2
+        print '+ ','- ' * box_dimensions,'+ ','- ' * box_dimensions,'+','- ' * box_dimensions,'+'
 
         for i in range(box_dimensions):
 
-            print '+ ',
+             print '| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| '
 
-            for i in range(dashes_in_boxes - 1):
-            
-                print '- ' * dashes_in_boxes,
+        print '+ ','- ' * box_dimensions,'+ ','- ' * box_dimensions,'+','- ' * box_dimensions,'+'
 
-                print '+ ',
-            
+        for i in range(box_dimensions):
 
-            print '+ '
+             print '| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| '
 
-    if box_dimensions % 2 != 0:
+        print '+ ','- ' * box_dimensions,'+ ','- ' * box_dimensions,'+','- ' * box_dimensions,'+'
 
-        print "box_dimensions is odd" 
+        for i in range(box_dimensions):
 
-    rows_and_grids  = box_dimensions
+             print '| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| '
 
-    #box_dimensions = (box_dimensions - 3)/2
+        print '+ ','- ' * box_dimensions,'+ ','- ' * box_dimensions,'+','- ' * box_dimensions,'+'
 
-    #print box_dimensions
-    
-    #print rows_and_grids
+print_3x3_grid(user_dimensions)
 
-    #if box_dimensions % 2 == 0 and box_dimensions != 2:
-    
-   # for i in range(box_dimensions):
-    
-   #     print '+ ',
-   #     print '- ' * box_dimensions,
-
-   # for i in range(box_dimensions):
-
-   #      print '| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| '
-
-   # print '+ ','- ' * box_dimensions,'+ ','- ' * box_dimensions,'+'
-
-   # for i in range(box_dimensions):
-
-#         print '| ','  ' * box_dimensions,'| ','  ' * box_dimensions,'| '
-#
-#    print '+ ','- ' * box_dimensions,'+ ','- ' * box_dimensions,'+'
-#        
-#        #return True
-#
-#    else: 
-#
-#        print "That's an even number, but the box must have an odd-numbered dimension"
-#        
-#        return
-#
-#print_grid(characters_wide_tall)
-
-print_3x3_grid(box_dimensions)
