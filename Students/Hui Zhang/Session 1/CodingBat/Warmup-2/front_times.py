ea71c0c97cafe0fc1cac3ead73f12305e4242d89
jerry zhang <jerzhang@localhost.localdomain>
2014-10-07 19:55:30 -0700

    My Python Homeworks

---
+++ Students/Hui Zhang/Session 1/CodingBat/Warmup-2/front_times.py
@@ -0,0 +1,5 @@
+def front_times(str, n):
+  if len(str) >= 3:
+      return(n*str[:3])
+  else:
+      return(n*str)
---
+++ Students/Hui Zhang/Session 1/fungrid-p1.py
@@ -0,0 +1,8 @@
+# my fungrid 
+def fungrid():
+    for i in range(0, 2):
+        print'+ - - - - + - - - - +'
+        for n in range(0, 4):
+            print'|         |         |'
+    print'+ - - - - + - - - - +'
+fungrid()
