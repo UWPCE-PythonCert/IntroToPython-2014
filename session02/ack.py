4e1410e9b92438c7ba2c4d710ee8a1f10ce6f820
Carolyn.Evans <carolyn.evans@Administrators-MacBook-Pro-2.local>
2014-10-11 14:17:39 -0700

    Initial checkin for Ackermann funtion

---
+++ session02/ack.py
@@ -0,0 +1,11 @@
+__author__ = 'carolyn.evans'
+
+def ack(m, n):
+    """ This is the Ackermann Function.
+    For a complete description, visit en.wikipedia.org/wiki/Ackermann_function.
+
+    :param m: Parameter m must be an integer value of zero or more.
+    :param n: Parameter m must be an integer value of zero or more.
+    :return:  If m and n are valid, an integer is returned.
+    """
+    
