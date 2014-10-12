de1962bb8b5090500c9ff93d2b58e0294806eccf
Ian M. Davis <ian.mack.davis@gmail.com>
2014-10-12 16:09:55 -0700

    Commit of series.py after adding a new function to evaluate the nth term of the lucas function.  Checked that it gives the correct result up to the 20th term of the Lucas fuction.  Added some basic assertion tests.

--- Students/imdavis/session02/series.py
+++ Students/imdavis/session02/series.py
@@ -7,7 +7,7 @@ def fibonacci(n):
     The Fibonnaci Series is defined as:
       0th term: 0
       1st term: 1
-      nth term: recursive fibonacci(n-1) + fibonacci(n)
+      nth term: sum of previous two terms in the series
 
     Args:
       n (int): must be >= 0
@@ -27,9 +27,36 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
+def lucas(n):
+    """Evaluation of the nth term of the Lucas Series. 
+
+    The Lucas Series is defined as:
+      0th term: 2
+      1st term: 1
+      nth term: sum of previous two terms in the series
+
+    Args:
+      n (int): must be >= 0
+
+    Yields:
+      nth term of the Lucas Series.
+
+    """
+
+    if (n < 0):
+        print "Arguments for the Lucas series must be >= 0."
+        return None
+    elif (n == 0):  # zeroth term = 0
+        return 2
+    elif (n == 1):  # first term = 1
+        return 1
+    else:
+        return lucas(n-1) + lucas(n-2)
+
 
 if __name__ == '__main__':
     FibanocciTestsPass = True
+    LucasTestsPass = True
 
     try:
         assert fibonacci(0) == 0
@@ -41,6 +68,16 @@ if __name__ == '__main__':
         FibanocciTestsPass = False
         print "All Fibonacci Tests Did Not Pass!"
 
-    if (FibanocciTestsPass):
+    try:
+        assert lucas(0) == 2
+        assert lucas(1) == 1
+        assert lucas(10) == 123
+        assert lucas(20) == 15127
+
+    except AssertionError:
+        LucasTestsPass = False
+        print "All Lucas Tests Did Not Pass!"
+
+    if (FibanocciTestsPass and LucasTestsPass):
         print "All Tests Pass!"
 
