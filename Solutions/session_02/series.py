fa121d3a2a412e6464bc7f5e0ec99ab08b951303
Anastomose <eric.buer@gmail.com>
2014-10-12 13:46:09 -1000

    Created draft fibbo function

---
+++ Solutions/session_02/series.py
@@ -0,0 +1,27 @@
+#@python: 2
+
+def fibonacci(n):
+    """Return the nth value in the Fibonacci series
+
+    Syntax:
+    fibonacci(n)
+
+    Arguments:
+    n -- a positive integer
+
+    The function calculates the specified value in the
+    Fibonacci series given by the argument "n".  For example
+    an n-value of 5 will return the value 3.
+    """
+    if n < 1:
+        return None
+    elif n:
+        return 0
+    else:
+        v1 = 0
+        v2 = 1
+        for r in range(n):
+            v3 = v1 + v2
+            v2 = v3
+            v1 = v2
+        return v3
