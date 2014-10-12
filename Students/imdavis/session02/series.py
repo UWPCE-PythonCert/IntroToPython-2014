162ed4d4d47226fea8fff3d91f5e51a1de199b19
Ian M. Davis <ian.mack.davis@gmail.com>
2014-10-12 15:51:48 -0700

    First commit of series.py function.  Calculates Fibanocci series up to 20th term correctly.  Contains a useful docstring and some assertions for some basic testing.

---
+++ Students/imdavis/session02/series.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+def fibonacci(n):
+    """Evaluation of the nth term of the Fibonnaci Series. 
+
+    The Fibonnaci Series is defined as:
+      0th term: 0
+      1st term: 1
+      nth term: recursive fibonacci(n-1) + fibonacci(n)
+
+    Args:
+      n (int): must be >= 0
+
+    Yields:
+      nth term of the Fibonnaci Series.
+
+    """
+
+    if (n < 0):
+        print "Arguments for the Fibonnaci series must be >= 0."
+        return None
+    elif (n == 0):  # zeroth term = 0
+        return 0
+    elif (n == 1):  # first term = 1
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+if __name__ == '__main__':
+    FibanocciTestsPass = True
+
+    try:
+        assert fibonacci(0) == 0
+        assert fibonacci(1) == 1
+        assert fibonacci(10) == 55
+        assert fibonacci(19) == 4181
+
+    except AssertionError:
+        FibanocciTestsPass = False
+        print "All Fibonacci Tests Did Not Pass!"
+
+    if (FibanocciTestsPass):
+        print "All Tests Pass!"
+
