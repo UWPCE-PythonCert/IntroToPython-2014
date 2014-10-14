4d2fce3851bea20bb4ceaf9d5696056f6ac12d7f
Anastomose <eric.buer@gmail.com>
2014-10-14 06:24:32 -1000

    Simplified fibo function.

--- Solutions/session_02/series.py
+++ Solutions/session_02/series.py
@@ -1,5 +1,3 @@
-#@python: 2
-
 def fibonacci(n):
     """Return the nth value in the Fibonacci series
 
@@ -11,19 +9,26 @@ def fibonacci(n):
 
     The function calculates the specified value in the
     Fibonacci series given by the argument "n".  For example
-    an n-value of 5 will return the value 3.
+    an n-value of 5 will return 3, the 5th value in the Fibonacci
+    series.
     """
-    if n < 1:
+
+    if n < 0:
         return None
-    elif n == 1:
+    elif not n:
         return 0
-    elif n == 2:
+    elif n == 1:
         return 1
     else:
-        v1 = 1
+        v1 = 0
         v2 = 1
+        v3 = 0
         for r in range(n):
-            v3 = v1 + v2
-            v2 = v3
+            v3 += v2
             v1 = v2
+            v2 = v3 - v1
         return v3
+
+for m in range(20):
+    print fibonacci(m),
+
