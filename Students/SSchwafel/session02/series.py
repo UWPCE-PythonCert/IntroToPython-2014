41d779719cca3c21df454b41427b268445b39dd4
smerdykov <mr.orange228@gmail.com>
2014-10-14 07:11:07 -0700

    Adding sum_series function, cleaning up Fib and Lucas

--- Students/SSchwafel/session02/series.py
+++ Students/SSchwafel/session02/series.py
@@ -6,6 +6,7 @@ user_number = raw_input('Which term do you want from the Fibonacci or Lucas sequ
 user_number = int(user_number)
 
 def fibonacci(n):
+    """Return the nth value of the Fibonacci sequence of numbers"""
 
     if n == 0:
         return 0
@@ -17,7 +18,8 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
     
 def lucas(n):
-    
+    """Return the nth value of the Lucas sequence of numbers"""
+
     if n == 0:
         return 2
 
@@ -28,8 +30,17 @@ def lucas(n):
         return lucas(n-1) + lucas(n-2)
 
 #print lucas(user_number)
-print fibonacci(user_number)
+#print fibonacci(user_number)
 
+def sum_series(n, optional1 = 0, optional2 = 1):
+
+    if optional1 == 0 and optional2 == 1:
+        return fibonacci(n)
+    if optional1 == 2 and optional2 == 1:
+        return lucas(n)
+
+    
+print sum_series(user_number,2,1)
 
     # This is where I am going to put my tests and asserts
 
@@ -39,6 +50,7 @@ if __name__ == "__main__":
     assert fibonacci(1) == 1
     assert fibonacci(3) == 2
     assert fibonacci(5) == 5
+
     print "\nAll Tests Pass!\n"
    # #
    # #
