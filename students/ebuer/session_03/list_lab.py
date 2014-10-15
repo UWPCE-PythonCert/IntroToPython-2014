c51a7e541aebae2899b8383c3344f2485f20fd5b
anastomose <eric.buer@gmail.com>
2014-10-15 13:55:09 -0700

    Updated list_lab.py with part 2

--- students/ebuer/session_03/list_lab.py
+++ students/ebuer/session_03/list_lab.py
@@ -1,5 +1,6 @@
 #@python: 2
 
+#part 1 of list lab
 listo = ['Apples', 'Pears', 'Oranges', 'Peaches']
 print listo
 
@@ -19,10 +20,38 @@ print 'Let me add something for you, like an Avocado.'
 listo = ['Avocado'] + listo
 print listo
 
-print 'And now a mango.'
+print '\nAnd now a mango.'
 listo.insert(0, 'Mango')
 print '%s\n' %listo
 
+print 'Can I share all the "P" fruits with you?'
 for fruit in listo:
     if 'P' in fruit[0]:
         print fruit
+
+#part 2 of list lab
+print "\nI'm still excited about our fruit list:"
+for fruit in listo:
+    print fruit
+
+print "\nBut I don't care for %s" %listo[-1]
+del listo[-1]
+print listo
+
+usr_rm = raw_input("Is there anything you don't care for? ")
+print "I can remove %s" %(usr_rm)
+
+spincycle = False
+while not spincycle:
+    if usr_rm in listo:
+        n = listo.count(usr_rm)
+        for m in range(n):  #need this since .remove only does 1 instance
+            listo.remove(usr_rm)
+        print '\nGreat, consider it gone.\n%s\n' %listo
+        spincycle = True
+    else:
+        print "\nSorry, I checked but %s isn't in our list.  I checked twice." %usr_rm
+        listo = listo * 2
+        listo.sort()
+        print '\n%s' %listo
+        usr_rm = raw_input("\nPlease check the list and let me remove something else. ")
