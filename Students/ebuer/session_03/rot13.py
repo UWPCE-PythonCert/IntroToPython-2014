0859d1d71d8b29d2d381d6613ff34efb68cdfd38
anastomose <anastomose@yahoo.com>
2014-10-16 14:25:04 -0700

    Started mailroom.py

--- Students/ebuer/session_03/rot13.py
+++ Students/ebuer/session_03/rot13.py
@@ -9,4 +9,4 @@ complete_key = s.maketrans(inkey, outkey)
 test_phrase = 'Zntargvp sebz bhgfvqr arne pbeare'
 print test_phrase.translate(complete_key)
 
-
+#Seems too easy! May try again using extended looping for practice.
