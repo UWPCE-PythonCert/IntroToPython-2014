abface16819331b20b28608bb6d03c3a59c30789
meshmote <deepcmd@gmail.com>
2014-10-16 21:27:31 -0700

    debugged list add/append statements

--- Students/RPerkins/session03/listlab.py
+++ Students/RPerkins/session03/listlab.py
@@ -1,4 +1,4 @@
-basket = ["Apples", "Pears", "Oranges", "Paches"]
+basket = ["Apples", "Pears", "Oranges", "Peaches"]
 print basket
 new_fruit = raw_input('Type in the name of another fruit-->')
 fruit = str(new_fruit)
@@ -6,8 +6,12 @@ basket.append(fruit)
 print basket
 fruit_index = raw_input("Type in the fruit index-->")
 index = int(fruit_index)
-print fruit_index +': ' + basket[index-1]
+print fruit_index + ': ' + basket[index-1]
 new_fruit = raw_input("Type in the name of another fruit-->")
 fruit = str(new_fruit)
-basket = fruit + basket
+basket = [fruit] + basket
+print basket
+new_fruit = raw_input("Type in the name of another fruit-->")
+fruit = str(new_fruit)
+basket.insert(0, fruit)
 print basket
