7dd833e58f733c960c90ba1d7eb4431475ecd832
meshmote <deepcmd@gmail.com>
2014-10-16 21:43:37 -0700

    Finished first action tasks and started second

--- Students/RPerkins/session03/listlab.py
+++ Students/RPerkins/session03/listlab.py
@@ -15,3 +15,9 @@ new_fruit = raw_input("Type in the name of another fruit-->")
 fruit = str(new_fruit)
 basket.insert(0, fruit)
 print basket
+for i in range(len(basket)):
+    if basket[i][0] == "P":
+        print basket[i]
+print basket
+basket.pop()
+print basket
