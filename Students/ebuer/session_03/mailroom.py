6d0c637ebe57ea69349d4af95f910293b05de218
anastomose <anastomose@yahoo.com>
2014-10-17 08:37:12 -0700

    Added name list function to mailroom

--- Students/ebuer/session_03/mailroom.py
+++ Students/ebuer/session_03/mailroom.py
@@ -5,30 +5,35 @@ import random
 def rd():
     return round(random.random() * 100, 2)
 
-#this may be a bad idea but we will see, a list of tuples for clients
+#this may be a bad idea but we will see
+#a list of tuples for donors and donations
 client_list = [
-    (('Askew', 'Anne'), (rd(), rd(), rd())),
-    (('Bocher', 'Joan'), (rd(), rd())),
-    (('Clarkson', 'Jeremy'), (rd())),
-    (('Hamont', 'Matthew'), (rd(), rd(), rd())),
-    (('May', 'James'), (rd(), rd())),
-    (('Parris', 'George van')] (rd(), rd(), rd()))]
+    (('Askew', 'Anne'), (87.50, 100, 200)),
+    (('Bocher', 'Joan'), (25, 43.27)),
+    (('Clarkson', 'Jeremy'), (10.03)),
+    (('Hamont', 'Matthew'), (1000, 250, 5)),
+    (('May', 'James'), (30, 75)),
+    (('Parris', 'George van'), (25, 35, 45))]
 
+#make a name list and a money list
+def list_maker(client_list):
+    donors = []
+    donations = []
+    for person in client_list:
+        donors.append(person[0])
+        donations.append(person[1])
+    return donors, donations
 
-
-
-
-
-
+donors, donations = list_maker(client_list)
 
 #take a name and check the list
-def client_check(nme, client_list):
+def client_check(nme, donors):
     fname, lname = nme.split(' ')
-    if (lname, fname) in client_list:
+    if (lname, fname) in donors:
         print '{f} {l} is a previous donor.'.format(f=fname, l=lname)
         return True
     else:
         print '{f} {l} is a not a previous donor.'.format(f=fname, l=lname)
         return False
 
-res=client_check('Annie Askew', client_list)
+res=client_check('Annie Askew', donors)
