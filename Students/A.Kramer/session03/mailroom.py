c7dd45ba33463e33a191ef2636d01b0f4778e392
Aleksey Kramer <voitel@gmail.com>
2014-10-16 18:32:17 -0700

    Finished mailroom.py

--- Students/A.Kramer/session03/mailroom.py
+++ Students/A.Kramer/session03/mailroom.py
@@ -1,5 +1,6 @@
 
 def execute():
+    """Run the main program """
     donors = [["Jon Doe", 50, 2], ["Jane Doe", 35, 7], ["Anonymous", 30, 10]]
     prompt = "Please, select the following options\n1 - Send a Thank you Letter\n2 - Create Report\n3 - Quit\n"
     while True:
@@ -17,16 +18,18 @@ def execute():
     print ""
             
 def print_report(d):
+    """Print users' report"""
     for donor in d:
         avg_amnt = 0
         if donor[2] == 0:
             avg_amnt = 0
         else:
             avg_amnt = donor[1]/donor[2]
-        print "Name: %s\tTotal Donated: $%2.2f\tNumber of Donations: %i\tAverage Donation: $%2.2f" % (donor[0], donor[2], donor[1], avg_amnt)
+        print "Name: %s\tTotal Donated: $%2.2f\tNumber of Donations: %i\tAverage Donation: $%2.2f" % (donor[0], donor[1], donor[2], avg_amnt)
     print ""
     
 def generate_letters(d):
+    """Generate Thank you letter"""
     name = select_donor(d)
     donation = get_donation(name)
     email_text = "Dear " + name + ",\n\nThank you very much for your generous donation of $" + str(donation) + ".\nThis donation will go a long way."
@@ -34,35 +37,37 @@ def generate_letters(d):
     print "\n======================================================================="
     print email_text
     print "=======================================================================\n"
-    for x in d:
-        if x[0] == name:
-            x[1] = x[1] + donation
-            x[2] = x[2] + 1
+    for k in d:
+        if k[0] == name:
+            k[1] = k[1] + float(donation)
+            k[2] = k[2] + 1
     
 def get_donation(n):
+    """Obtain donation ammount"""
     while True:
         amount = raw_input("\nProvide donation amount: ")
-        if float(amount):
-            return float(amount)
-        else:
-            print "Invalid Number"
-            print ""
+        try:
+            if float(amount):
+                return float(amount)
+        except ValueError:
+            print "Invalid Number provided for donation"
 
 def select_donor(d):
+    """Obtain Donor name to use"""
     while True:
         n = raw_input("\nType the name of the donor or type the work 'list': ")
         if n == "list":
+            print n == "list"
             for x in d:
                 print x[0]
         else:
-            for x in d:
-# The comparison below does not work.  I do not understand why....
-                if x[0] == n:
+            for g in d:
+                if g[0] == n:
                     return n
-                else:
-                    d.append([n, 0, 0])
-                    return n
+            d.append([n, 0, 0])
+            return n
 
 if __name__ == "__main__":
+    """Execute the main program"""
     execute()
     
