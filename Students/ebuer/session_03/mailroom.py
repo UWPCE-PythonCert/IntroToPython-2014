c9a27916b5e6c85e049f0d94585f470d51572451
anastomose <anastomose@yahoo.com>
2014-10-17 09:27:10 -0700

    Started interactive looping.

--- Students/ebuer/session_03/mailroom.py
+++ Students/ebuer/session_03/mailroom.py
@@ -1,10 +1,5 @@
 #mailroom program to break the monotony
 
-import random
-
-def rd():
-    return round(random.random() * 100, 2)
-
 #this may be a bad idea but we will see
 #a list of tuples for donors and donations
 client_list = [
@@ -30,10 +25,35 @@ donors, donations = list_maker(client_list)
 def client_check(nme, donors):
     fname, lname = nme.split(' ')
     if (lname, fname) in donors:
-        print '{f} {l} is a previous donor.'.format(f=fname, l=lname)
+        print '{f:s} {l} is a previous donor.'.format(f=fname, l=lname)
         return True
     else:
-        print '{f} {l} is a not a previous donor.'.format(f=fname, l=lname)
+        print '{f:s} {l} is a not a previous donor.'.format(f=fname, l=lname)
         return False
 
-res=client_check('Annie Askew', donors)
+res=client_check('Anne Askew', donors)
+
+
+#mailroom control looping
+bored = True
+
+while bored:
+    print 'Welcome to the Mailroom timesaver! Please choose an option:\n\
+            0: Send a thank you\n\
+            1: Create a report\n'
+
+    report_opt = int(raw_input('Please enter your choice: '))
+
+    if report_opt == 0 : ## sending a thank you
+        usr_name = raw_input('Please enter a name: ')
+        
+        if client_check(usr_name, donors):
+            usr_donation = raw_input('Please enter a donation: ')
+        else:
+            fname, lname = usr_name.split(' ')
+            client_list.append(((lname, fname), (None)))
+
+
+
+
+
