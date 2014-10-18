0974c93a9fb26a34ac69a1b3182c01876a4ded2f
meshmote <deepcmd@gmail.com>
2014-10-17 20:13:40 -0700

    Added action #3 code

--- Students/RPerkins/session03/listlab.py
+++ Students/RPerkins/session03/listlab.py
@@ -1,3 +1,5 @@
+
+#action #1
 basket = ["Apples", "Pears", "Oranges", "Peaches"]
 print basket
 new_fruit = raw_input('Type in the name of another fruit-->')
@@ -18,6 +20,33 @@ print basket
 for i in range(len(basket)):
     if basket[i][0] == "P":
         print basket[i]
+
+#action 2
 print basket
 basket.pop()
 print basket
+del_fruit = raw_input("Pick a fruit from the list to delete-->")
+fruit = str(del_fruit)
+if fruit in basket:
+    for i in range(len(basket)):
+        if basket[i] == fruit:
+            basket.pop(i)
+else:
+    print 'That fruit is not in the basket'
+
+#action 3
+deletes = 0
+for i in range(len(basket)):
+    i = i - deletes
+    lfruit = None
+    while not (lfruit == "yes" or lfruit == "no"):
+        like_fruit = raw_input("Do you like %s-->" % basket[i].lower())
+        lfruit = str(like_fruit)
+    if lfruit == 'no':
+        basket.pop(i)
+        deletes += 1
+print basket
+
+
+
+
