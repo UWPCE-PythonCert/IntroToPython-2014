9099f9990169f421152d4ebd9c985295fcb18ab4
Bryan Davis <bryan@neoty.pe>
2014-10-19 11:25:15 -0700

    another update

--- Students/brydavis/session03/rot13.py
+++ Students/brydavis/session03/rot13.py
@@ -2,21 +2,66 @@
 
 from sys import argv
 
-s =  argv[1] if len(argv) > 1 else 'Hello World!'
 
-def rot13(s=s):
+s = argv[1] if len(argv) > 1 else 'Hello World!'
+a = int(argv[2]) if len(argv) > 2 else 13
+
+
+def rot13(s=s,a=a):
 	hashed = ''
 
 	for i in s:
-		hashed += chr(ord(i)+13)
+		c = ord(i)+a
+		# print c
+		hashed += chr(c) if c < 256 else chr(c - 255)
 
 	return hashed
 
 
+def decrypt (s=s,a=a):
+	hashed = ''
+
+	for i in s:
+		c = ord(i)-a
+		# print c
+		hashed += chr(c) if c > -1 else chr(c + 255)
+
+	return hashed
+
+def try_all_decrypt1(s=s):
+	
+	for n in range(256):
+		hashed = ''
+
+		for i in s:
+			c = ord(i)-n
+			hashed += chr(c) if c > -1 else chr(c + 255)
+	
+		print n, hashed
+
+
+def try_all_decrypt2(s=s):
+	
+	for n in range(256):
+		hashed = ''
+
+		for i in s:
+			c = ord(i)+n
+			hashed += chr(c) if c < 256 else chr(c - 255)
+	
+		print n, hashed
+
+
+
+
 if __name__ == '__main__':
 
-	print 'Testin ROT13 encryption...'
+	print 'Testin ROT13 encryption...\n'
 	assert rot13()
+	assert decrypt()
+
+	h = rot13()
 
 	print 'Orignal text: %s' % s
-	print 'Encrypted text: %s' % rot13()
+	print 'Encrypted hash: %s' % h
+	print 'Decrypted hash: %s' % decrypt(h)
