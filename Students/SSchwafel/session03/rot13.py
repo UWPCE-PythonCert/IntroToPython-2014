0ce66ddc502bcdea28614fc674e49bb24a72fcf1
smerdykov <mr.orange228@gmail.com>
2014-10-19 17:24:51 -0700

    Fixing if statements to run with upper and lowercase letters

--- Students/SSchwafel/session03/rot13.py
+++ Students/SSchwafel/session03/rot13.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python
-user_string = raw_input("Please enter the string you'd like to decrypt with Rot13 ")
+user_string = raw_input("Please enter the string you'd like to encrypt/decrypt with Rot13 ")
 
 
 #Take a user string
+
 def rot13(x):
     """Takes a letter and moves forward 13 characters"""
 
-    x = int( ord(x ))
-    x = x+13
+    x = int(ord(x))
 
     if x < 65:
         return ''
-    if x >= 65 and x<=90:
-        x = chr((x - 90) + 65)
 
-    elif x > 122:
-        x = chr((x - 122) + 97)
-    print x
-        #do some rot13 decrypting here
+    elif x >= 65 and x<=90:
+
+        x = x + 13
+
+        if x > 90:
+            x = x - 26
+        return chr(x)
+
+    elif x >= 97 and x <= 122:
+	x = x + 13
+        if x > 122:
+            x = x - 26
+
+        return chr(x)
+
+        #x = (x - 122) + 97
 
 
 
 
-rot13(user_string)
+print rot13(user_string)
+
+#User_string = list(user_string)
+
+
+#For i in user_string:
+#    print i
+#
