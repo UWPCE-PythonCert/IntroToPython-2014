38880352a545939c7ba0010eb52806910c243885
meshmote <deepcmd@gmail.com>
2014-10-21 21:05:28 -0700

    started dict labs

---
+++ Students/RPerkins/session04/dict_setlab.py
@@ -0,0 +1,24 @@
+__author__ = 'Robert W. Perkins'
+
+
+#d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+#print d
+#d.pop('cake')
+#print d
+#d['fruit'] = 'Mango'
+#print d
+#print d.keys()
+#print d.values()
+#print 'cake' in d
+#print 'Mango' in d.values()
+
+int_list = []
+hex_list = []
+for i in range(16):
+    int_list.append(i)
+    hex_list.append(hex(i))
+
+h_ex={}
+for k, l in zip(int_list, hex_list):
+    h_ex[k] = l
+print h_ex
