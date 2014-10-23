66a38f4676bf7cbb3e774cf70fe91a8b7eaf1c09
meshmote <deepcmd@gmail.com>
2014-10-22 18:39:59 -0700

    Added, debugged dict problem code

--- Students/RPerkins/session04/dict_setlab.py
+++ Students/RPerkins/session04/dict_setlab.py
@@ -1,16 +1,16 @@
 __author__ = 'Robert W. Perkins'
 
 
-#d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
-#print d
-#d.pop('cake')
-#print d
-#d['fruit'] = 'Mango'
-#print d
-#print d.keys()
-#print d.values()
-#print 'cake' in d
-#print 'Mango' in d.values()
+d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+print d
+d.pop('cake')
+print d
+d['fruit'] = 'Mango'
+print d
+print d.keys()
+print d.values()
+print 'cake' in d
+print 'Mango' in d.values()
 
 int_list = []
 hex_list = []
@@ -22,3 +22,8 @@ h_ex={}
 for k, l in zip(int_list, hex_list):
     h_ex[k] = l
 print h_ex
+
+d_prime = {}
+for k, v in d.items():
+    d_prime[k] = v.count('t')
+print d_prime
