22aaa7ca4a8493fef91986877023980400c38549
meshmote <deepcmd@gmail.com>
2014-10-23 20:33:50 -0700

    Built, tested get_book function

--- Students/RPerkins/session04/kata14.py
+++ Students/RPerkins/session04/kata14.py
@@ -1,11 +1,13 @@
 __author__ = 'Robert W. Perkins'
 
 
-def get_book():
-    f = open('secrets.txt')
-    secret_data = f.read()
+def get_book(target):
+    f = open(target)
+    book_data = f.read()
     f.close()
+    return book_data
 
 
 if __name__ == '__main__':
-    get_book()
+    source_text = '/intropython/data/sherlock_small.txt'
+    print get_book(source_text)
