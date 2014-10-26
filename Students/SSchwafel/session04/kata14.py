3a52f176f88c50b084acc33ed1aeb37ddda10a51
saschwafel <mr.orange228@gmail.com>
2014-10-26 12:30:22 -0700

    Adding function to grab first three values and add them to dictionary

--- Students/SSchwafel/session04/kata14.py
+++ Students/SSchwafel/session04/kata14.py
@@ -10,6 +10,33 @@ book_file.close()
 
 text_doc = text_doc.replace('--',' ')
 
-print list(text_doc.split())
+text_doc = list(text_doc.split())
+
+print text_doc
+
+dict_words = {}
 
 
+#for i in text_doc:
+
+def take_first_three_values(x):
+    """Takes first three values from text document, turns val.1 into key, second two into a list of items"""
+    #grabs first value from text doc, removes it
+    key = x.pop(0)
+    #first value has now switched to second word, removes that one too
+
+    word1 = x.pop(0)
+    #first value has now switched to third word, removes that one too
+
+    word2 = x.pop(0)
+
+    #add a dictionary entry for the first three values
+    dict_words[key] = [word1,word2]
+
+take_first_three_values(text_doc)
+
+print '\n'
+print "Here's the dictionary entry list so far:\n"
+print dict_words
+print "Here's the original entry list with values removed:\n"    
+print text_doc
