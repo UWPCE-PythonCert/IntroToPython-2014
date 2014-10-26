a3c72956f87dd10f6c2513147adf57ea9d6b6195
meshmote <deepcmd@gmail.com>
2014-10-25 17:45:43 -0700

    Restored for loops to add in newlines and punctuation at end

--- Students/RPerkins/session04/kata14.py
+++ Students/RPerkins/session04/kata14.py
@@ -1,6 +1,7 @@
 __author__ = 'Robert W. Perkins'
 import random
 
+
 def get_book(target):
     """ Open target file and read contents into book_data"""
     f = open(target)
@@ -38,6 +39,11 @@ def create_dict(orig_text):
     return trigram
 
 
+def get_randomkey(t_gram):
+    """Return a random key"""
+    return random.choice(list(t_gram.keys()))
+
+
 def get_newword(word_key, word_dict):
     """Return a random word from the list at the provided key"""
     new_wordlist = word_dict.get(word_key)
@@ -46,15 +52,32 @@ def get_newword(word_key, word_dict):
 
 def create_newbook(trigram_dict, word_limit, w_line):
     """Create random output of num_words words, using trigram_dict keys to generate new words"""
-
-    start_key = random.choice(list(trigram_dict.keys()))
+    start_key = get_randomkey(trigram_dict)
     new_word = get_newword(start_key, trigram_dict)
 
-    print start_key
-    print type(start_key)
-    print new_word
-    print type(new_word)
-    return None
+    #print start_key
+    out_list = start_key.split(' ')
+    out_list.append(new_word)
+    #print out_list
+    left_frame = 0
+
+    for i in range(word_limit):
+        for j in range(w_line):
+            next_key = '%s %s' % (out_list[left_frame+1], out_list[left_frame + 2])
+                #print next_key in trigram_dict
+            while not next_key in trigram_dict:
+                next_key = get_randomkey(trigram_dict)
+            next_word = get_newword(next_key, trigram_dict)
+            out_list.append(next_word)
+            left_frame += 1
+        out_list.append('\n')
+    out_list.append('.')
+    out_string = ' '.join(out_list)
+    return out_string
+
+    #print new_word
+    #print type(new_word)
+    #print out_list
 
 
 if __name__ == '__main__':
@@ -62,8 +85,11 @@ if __name__ == '__main__':
     # words_line gives the number of words per line
     num_words = 200
     words_line = 20
-    source_text = '/intropython/data/sherlock_small.txt'
+    #source_text = '/intropython/data/sherlock_small.txt'
+    source_text = '/intropython/data/sherlock.txt'
 
     new_inbook = get_book(source_text)
     new_dict = create_dict(new_inbook)
+    #print new_dict
     new_outbook = create_newbook(new_dict, num_words, words_line)
+    print new_outbook
