68be21a3bed4eb6d57e9ec590824fac562378eec
Salim Hamed <salimhamed@gmail.com>
2014-10-28 00:16:43 -0700

    Finished first version of Trigrams.

--- Students/salim/session04/kata_14.py
+++ Students/salim/session04/kata_14.py
@@ -1,5 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 
+import string
+
 
 def file_to_list(a_file, start_line=0, end_line=0):
     """Read lines of file into a list. Ignore blank and all cap lines."""
@@ -43,7 +45,7 @@ def build_trigram(all_words):
 
             # build keys and values for dict
             key = '{:s} {:s}'.format(word, all_words[idx + 1])
-            value = all_words[idx + 3]
+            value = all_words[idx + 2]
 
             # get or set the current_set for the given key
             current_set = trigram_dict.setdefault(key, set([value]))
@@ -54,33 +56,65 @@ def build_trigram(all_words):
     return trigram_dict
 
 
-def print_trigram(trigram, start_words, num_of_words):
+def get_trigram_output(trigram, start_words, num_of_words):
     """Print trigram."""
-    print start_words
+    trigram_output = start_words.split()
+    key_string = '{:s} ' * 2
+    key_string = key_string.strip()
 
     for i in range(num_of_words):
-        next_words = trigram.get(start_words)
-        if next_words:
-            start_words = next_words.pop()
-            print start_words ,
-        else:
+        # build the key to lookup the set of next words from dictionary
+        key = key_string.format(*trigram_output[i:i+2])
+
+        # get the set of next words
+        try:
+            return_set = trigram[key]
+        except KeyError:
             break
 
+        # randomly choose and remove a word from the set
+        next_word = return_set.pop()
+
+        # add the new work to the output list
+        trigram_output.append(next_word)
+
+    return trigram_output
+
+
+def clean_string(s, lowercase = False, punctuation = False):
+    s = s.strip()  # strip whitespce
+
+    if lowercase:
+        s = s.lower()  # convert to lowercase
+
+    if punctuation:
+        # create list with all punctuation
+        delete_characters = list(string.punctuation)
+
+        # exclude some punctuation from being removed
+        delete_characters.remove('!')
+        delete_characters.remove('.')
+        delete_characters.remove('?')
+        delete_characters.remove(',')
+        delete_characters.remove("'")
+
+        s = s.translate(None, ''.join(delete_characters))
+
+    return s
 
 if __name__ == '__main__':
 
-    # define variables
-    # path_book = ('/Users/salimhamed/Documents/Documents/School/'
-    #             'Python (2014)/downloads/sherlock_small.txt')
-
     path_book = ('/Users/salimhamed/Documents/Documents/School/'
                  'Python (2014)/downloads/sherlock.txt')
+
+    # path_book = ('/Users/salimhamed/Documents/Documents/School/'
+    #             'Python (2014)/downloads/sherlock_small.txt')
 
     start_line = 0  # starting line of the file
     end_line = 0  # ending line of the file
 
     num_of_words_to_print = 200
-    start_words = 'I have'
+    start_words = 'I should'
 
     # read file to list
     f = open(path_book)
@@ -90,8 +124,7 @@ if __name__ == '__main__':
     f_string = ' '.join(f_list)
 
     # strip white space and remove quotes
-    f_string = f_string.strip()
-    f_string = f_string.replace('"', '')
+    f_string = clean_string(f_string, lowercase = False, punctuation = False)
 
     # split single string into list of words
     words_list = f_string.split()
@@ -99,5 +132,12 @@ if __name__ == '__main__':
     # build a trigram
     trigram_dict = build_trigram(words_list)
 
-    # print trigram
-    print_trigram(trigram_dict, start_words, num_of_words_to_print)
+    # get trigram list
+    trigram_list = get_trigram_output(trigram_dict,
+                                      start_words,
+                                      num_of_words_to_print)
+
+    # print trigram list
+    print_string = ('{:s} ' * len(trigram_list))[:-1]
+    print 'Trigram of {:d} words:'.format(len(trigram_list))
+    print print_string.format(*trigram_list)
