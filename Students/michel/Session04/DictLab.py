28daa9f423c3a44742839e9092b4df7a0a4eef58
QuietMike <QuietMike08@gmail.com>
2014-11-03 20:59:51 -0800

    Added Session04 and Session05 folders

---
+++ Students/michel/Session04/DictLab.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 21 19:25:20 2014
+
+@author: Michel
+"""
+
+d = {'name':'Chris', 'city':'Seattle', 'cake':'chocolate'}
+print d
+d.pop('cake')
+print d
+d['fruit'] = 'mango'
+print d
+print d.keys()
+print d.values()
+print 'cake' in d
+print 'mango' in d.values()
+nb = range(15)
+hexa = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E']
---
+++ Students/michel/Session05/argLab.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 28 19:21:02 2014
+
+@author: Michel
+"""
+
+def colorTest(fore_color = 'Black', back_color = 'White', link_color = 'Blue',
+              visited_color = 'Red'):
+              
+              print fore_color, back_color, link_color, visited_color
+              
+              
