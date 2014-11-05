277404d57fae9715f8eb6846e760ed4c0d71aefe
Schuyler Schwafel <saschwafel@gmail.com>
2014-11-04 21:07:52 -0800

    Adding this week's lab and notes

---
+++ Students/SSchwafel/session06/html_render.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+class element(object):
+    
+    def __init__(self, tag_name, html):
+        
+        self.tag_name = 'html'
+        self.indentation = '    '
+    
+
+print "\nblah"
+
+cat = 'cat'
+print element.tag_name(cat)
