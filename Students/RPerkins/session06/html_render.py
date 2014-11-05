58009b9e4c1940fe95adbe67a9746da556ddd040
meshmote <deepcmd@gmail.com>
2014-11-04 21:10:52 -0800

    Set up html_render files, added class inits to Element

---
+++ Students/RPerkins/session06/html_render.py
@@ -0,0 +1,25 @@
+__author__ = 'Robert W. Perkins'
+
+
+#!/usr/bin/env python
+
+"""
+Python class example.
+"""
+
+
+# The start of it all:
+# Fill it all in here.
+class Element(object):
+
+    tag = '<html>'
+    indent = 5
+
+    def __init__(self, content=None):
+        pass
+
+    def append(self, new_content):
+        pass
+
+    def render(self, file_out, ind=""):
+        pass
