6d3a2ac09a695100c8c3a969fe3fcdb24778f620
meshmote <deepcmd@gmail.com>
2014-11-04 19:40:38 -0800

    Added, debugged extra credit function

--- Students/RPerkins/session06/lambda_lab.py
+++ Students/RPerkins/session06/lambda_lab.py
@@ -7,8 +7,19 @@ def lam_lab(n):
         l.append(lambda x, e=i: x+e)
     return l
 
+
+def lam_lab2(num):
+
+    t = []
+    [(t.append(lambda x, e=i: x+e)) for i in range(num)]
+    return t
+
 if __name__ == '__main__':
 
-    out_lab = lam_lab(10)
-    for f in out_lab:
-        print f(3)
+    func_outlab = lam_lab(10)
+    for f in func_outlab:
+        print f(4)
+
+    comp_outlab = lam_lab2(10)
+    for g in comp_outlab:
+        print g(4)
