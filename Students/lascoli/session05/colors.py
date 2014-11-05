50647a0f96f69370900f6277f5de8da81738f2f2
LouA <lascoli2002@yahoo.com>
2014-11-04 17:39:36 -0800

    Session05 Tasks

--- Students/lascoli/session05/colors.py
+++ Students/lascoli/session05/colors.py
@@ -1,6 +1,12 @@
-def color_fun(*args, **kwargs):
-   print ('The foreground color is:',args[0])
-   print ('The background  color is:',args[1])
-   print ('The link color is:', kwargs)
-   print ('The visited link color is:', kwargs)
-color_fun('black', 'red', link_color='blue', visited_link_color='yellow')
+colors =("black","red")
+colors2 = dict(link_color ="yellow",visited_link_color="green")
+
+def color_fun(*argv,**kwargs):
+    
+    print "\nThe fore_color is",argv[0],"\n"
+    print "The back_color is",argv[1],"\n"
+    print "The link_color is", kwargs["link_color"],"\n"
+    print "The visited_link_color is",kwargs["visited_link_color"],"\n"
+    
+ 
+color_fun(*colors,**colors2) 
