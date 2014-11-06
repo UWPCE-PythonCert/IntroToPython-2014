60b48c35111b9220ae4973a3d21e78481859a84c
meshmote <deepcmd@gmail.com>
2014-11-05 21:01:33 -0800

    Changed content data structure to list, rewrote append and render methods, debugged

--- Students/RPerkins/session06/html_render.py
+++ Students/RPerkins/session06/html_render.py
@@ -8,26 +8,32 @@ Python class example.
 """
 
 
-# The start of it all:
-# Fill it all in here.
 class Element(object):
 
     tag = '<html>'
+    endtag = tag[:1] + '/' + tag[1:]
     indent = '    '
 
     def __init__(self, content=None):
 
-        self.content = content
+        self.content = [content]
 
     def append(self, new_content):
+        """Check for existing content and add new_content"""
 
-        if self.content is None:
-            self.content = new_content
+        if self.content[0] is None:
+            self.content = [new_content]
         else:
-            self.content = '{ex_content}{n_content}'.format(ex_content=self.content, n_content=new_content)
+            self.content.append(new_content)
 
     def render(self, file_out, ind=""):
         """Write content and tags to the file_out StringIO object"""
-        file_out.write('{tag_indent}{start_tag}\n{element_indent}{content}\n{tag_indent}{end_tag}'.format
-                       (tag_indent=self.indent, start_tag=self.tag, element_indent=ind+self.indent, content=self.content,
-                        end_tag=self.tag))
+
+        file_out.write('{tag_indent}{start_tag}\n'.format
+                            (tag_indent=self.indent, start_tag=self.tag,))
+
+        file_out.writelines('{element_indent}{content}\n'.format
+                            (element_indent=ind+self.indent, content=item) for item in self.content)
+
+        file_out.write('{tag_indent}{end_tag}'.format
+                            (tag_indent=self.indent, end_tag=self.endtag))
