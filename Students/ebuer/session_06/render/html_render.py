75cfb8346b8b9d10f32a969338aa4f935464305b
Anastomose <anastomose@yahoo.com>
2014-11-05 21:37:10 -0800

    Working on render class methods

--- Students/ebuer/session_06/render/html_render.py
+++ Students/ebuer/session_06/render/html_render.py
@@ -6,14 +6,20 @@ html_render.py
 
 
 class Element(object):
+    ind = 4  # tag for number of spaces in ind
+    tag = "<html>"
 
-    def __init__(self, html, indtag, content=None):
-        self.html = html
-        self.ind = indtag
+    def __init__(self, content=None):
         self.content = content
 
-    def append(self, new_content):
-        self.content = " ".join(self.content, new_content)
+    def append(self, new_content=""):
+        if self.content is None:
+            self.content = new_content
+        else:
+            self.content = " ".join([self.content, new_content])
 
+    # renders the tag and the strings in the content
     def render(self, file_out, ind=""):
-        pass
+        self.file_out = file_out
+        # fid = open(file_out, 'w')
+
