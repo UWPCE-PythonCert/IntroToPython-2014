987f88d2d4b856f97983de1c65c287985d76c508
meshmote <deepcmd@gmail.com>
2014-11-05 19:50:33 -0800

    Fixed tag indentation, fixed None append

--- Students/RPerkins/session06/html_render.py
+++ Students/RPerkins/session06/html_render.py
@@ -13,13 +13,21 @@ Python class example.
 class Element(object):
 
     tag = '<html>'
-    indent = 5
+    indent = '    '
 
     def __init__(self, content=None):
-        pass
+
+        self.content = content
 
     def append(self, new_content):
-        pass
+
+        if self.content is None:
+            self.content = new_content
+        else:
+            self.content = '{ex_content}{n_content}'.format(ex_content=self.content, n_content=new_content)
 
     def render(self, file_out, ind=""):
-        pass
+        """Write content and tags to the file_out StringIO object"""
+        file_out.write('{tag_indent}{start_tag}\n{element_indent}{content}\n{tag_indent}{end_tag}'.format
+                       (tag_indent=self.indent, start_tag=self.tag, element_indent=ind+self.indent, content=self.content,
+                        end_tag=self.tag))
