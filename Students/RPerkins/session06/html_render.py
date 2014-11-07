e35b351559f6d77a6dc2d23728edff7014654060
meshmote <deepcmd@gmail.com>
2014-11-06 22:06:49 -0800

    Added p and body classes and extended render method.  Partial debug of tag repetition

--- Students/RPerkins/session06/html_render.py
+++ Students/RPerkins/session06/html_render.py
@@ -29,10 +29,10 @@ class Element(object):
     def render(self, file_out, ind=""):
         """Write content and tags to the file_out StringIO object"""
 
-        #file_out.write('{tag_indent}{start_tag}\n'.format
-                      #(tag_indent=self.indent, start_tag=self.tag,))
+        #file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
 
         for item in self.content:
+            #file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
             if type(item) is str:
                 file_out.write('{tag_indent}{start_tag}\n{element_indent}{content}\n{tag_indent}{end_tag}\n'.format
                               (tag_indent=self.indent, start_tag=self.tag, element_indent=ind+self.indent,
@@ -42,7 +42,7 @@ class Element(object):
                 item.render(file_out, "    ")
                 file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
 
-        #file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
+                #file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
 
 
 class Html(Element):
