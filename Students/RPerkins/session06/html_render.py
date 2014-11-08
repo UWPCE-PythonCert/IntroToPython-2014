a646407f5a631831b5c73334c6f984728145dba0
meshmote <deepcmd@gmail.com>
2014-11-07 19:09:14 -0800

    Debugged step 2

--- Students/RPerkins/session06/html_render.py
+++ Students/RPerkins/session06/html_render.py
@@ -29,20 +29,18 @@ class Element(object):
     def render(self, file_out, ind=""):
         """Write content and tags to the file_out StringIO object"""
 
-        #file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
-
-        for item in self.content:
-            #file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
-            if type(item) is str:
+        if type(self) is Body:
+            file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
+            for item in self.content:
                 file_out.write('{tag_indent}{start_tag}\n{element_indent}{content}\n{tag_indent}{end_tag}\n'.format
-                              (tag_indent=self.indent, start_tag=self.tag, element_indent=ind+self.indent,
-                               content=item, end_tag=self.endtag))
-            else:
+                              (tag_indent=item.indent, start_tag=item.tag, element_indent=ind+item.indent,
+                               content=item.content[0], end_tag=item.endtag))
+            file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
+        else:
+            for item in self.content:
                 file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
                 item.render(file_out, "    ")
                 file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
-
-                #file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
 
 
 class Html(Element):
