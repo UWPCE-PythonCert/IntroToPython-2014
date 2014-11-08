f5ebdaeb96e621132e67a5284037f56af3d83a2f
meshmote <deepcmd@gmail.com>
2014-11-07 19:34:55 -0800

    Moved class specific render method code from superclass to subclass overrides

--- Students/RPerkins/session06/html_render.py
+++ Students/RPerkins/session06/html_render.py
@@ -27,20 +27,12 @@ class Element(object):
             self.content.append(new_content)
 
     def render(self, file_out, ind=""):
-        """Write content and tags to the file_out StringIO object"""
+        """Write tags, call render method on content objects, and write to the file_out StringIO object"""
 
-        if type(self) is Body:
+        for item in self.content:
             file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
-            for item in self.content:
-                file_out.write('{tag_indent}{start_tag}\n{element_indent}{content}\n{tag_indent}{end_tag}\n'.format
-                              (tag_indent=item.indent, start_tag=item.tag, element_indent=ind+item.indent,
-                               content=item.content[0], end_tag=item.endtag))
+            item.render(file_out, "    ")
             file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
-        else:
-            for item in self.content:
-                file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
-                item.render(file_out, "    ")
-                file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
 
 
 class Html(Element):
@@ -55,9 +47,31 @@ class P(Element):
     endtag = '</p>'
     indent = '            '
 
+    def render(self, file_out, ind=""):
+        """Write paragraph content and tags to the file_out StringIO object"""
+
+        file_out.write('{tag_indent}{start_tag}\n{element_indent}{content}\n{tag_indent}{end_tag}\n'.format
+                      (tag_indent=self.indent, start_tag=self.tag, element_indent=ind+self.indent,
+                       content=self.content[0], end_tag=self.endtag))
+
 
 class Body(Element):
 
     tag = '<body>'
     endtag = '</body>'
+    indent = '        '
+
+    def render(self, file_out, ind=""):
+        """Write body tags and call render method on each P object"""
+
+        file_out.write('{tag_indent}{start_tag}\n'.format(tag_indent=self.indent, start_tag=self.tag,))
+        for item in self.content:
+            item.render(file_out, "    ")
+        file_out.write('{tag_indent}{end_tag}\n'.format(tag_indent=self.indent, end_tag=self.endtag))
+
+
+class Head(Element):
+
+    tag = '<head>'
+    endtag = '</head>'
     indent = '        '
