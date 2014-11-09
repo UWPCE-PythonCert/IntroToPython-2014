03094c7c00877a7cd719cd597260220aa2bd7ca8
Anastomose <anastomose@yahoo.com>
2014-11-08 18:30:21 -0800

    Funcfile working correctly at cmd

--- Students/ebuer/session_06/funcfile/funcfile.py
+++ Students/ebuer/session_06/funcfile/funcfile.py
@@ -1,23 +1,23 @@
-# coding: utf-8 
+# coding: utf-8
 """
 Write a program that takes a filename and “cleans” the file by removing all the leading and trailing whitespace from each line.
     Read in the original file and write out a new one, either creating a new file or overwriting the existing one.
     Give your user the option of which to perform.
     Use map() to do the work.
     Write a second version using a comprehension.
-    Use sys.argv to hold the command line arguments the user typed in. If the user types: $ python the_script a_file_name
 
-Then:
-    import sys
-    filename = sys.argv[1]
-    will get filename == "a_file_name"
+    Use sys.argv to hold the command line arguments the user typed in.
+    If the user types: $ python the_script a_file_name then:
+        import sys
+        filename = sys.argv[1]
+        will get filename == "a_file_name"
 """
 
 from io import open, StringIO
 import sys
 
 # Read in the original file and return contents
-def gettext(infile='junkfile.txt'):
+def gettext(infile):  # ='junkfile.txt'
     fid = StringIO()
     with open(infile, 'r', encoding='UTF-8') as f:
         fid = f.readlines()
@@ -27,6 +27,10 @@ def gettext(infile='junkfile.txt'):
 def spstrip(fid):
     return map(lambda x: x.strip(' \n'), fid)
 
+# use a list comprehension instead of the map function
+def compstrip(fid):
+    return [x.strip(' \n') for x in fid]
+
 # print a new file
 def pttext(outfile, intext):
     with open(outfile, 'w', encoding='UTF-8') as f:
@@ -34,6 +38,17 @@ def pttext(outfile, intext):
 
 
 # Putting together the functions into a script
-filetext=gettext('junkfile.txt')
-cleantext=spstrip(filetext)
-pttext('junkfile2.txt', cleantext)
+fid = sys.argv[1]  # note that argv does not count 'python' at prompt
+filetext = gettext(fid)
+cleantext = spstrip(filetext)
+# cleantext = compstrip(filetext)
+
+handle = raw_input('Overwrite existing? Enter Y/N ')
+
+if handle is "Y" or handle is 'y':
+    pttext(fid, cleantext)
+elif handle is "N" or handle is 'n':
+    fname = fid.split('.')
+    pttext("_".join([fname[0], 'clean.txt']), cleantext)
+else:
+    print 'Sorry, that is not possible'
