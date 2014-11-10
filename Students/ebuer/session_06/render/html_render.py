5ac711ee28770a966ae5053c21265ee5f604e6a6
Anastomose <anastomose@yahoo.com>
2014-11-09 21:56:29 -0800

    Render part 2, not working.

--- Students/ebuer/session_06/render/html_render.py
+++ Students/ebuer/session_06/render/html_render.py
@@ -1,16 +1,13 @@
+#coding: utf-8
 """
 html_render.py
-
-Questions: how do I inspect class and object attributes to see if the correct value has been ascribed?
 
 """
 
 # starting render classes
-
-
 class Element(object):
-    indent = 0  # indent level for tag
-    tag = [u"<html>", u"</html>"]
+    indent = ''  # indent level for tag
+    tag = u"html"
 
     def __init__(self, content=None):
         self.content = content
@@ -22,18 +19,27 @@ class Element(object):
             self.content.append(new_content)
 
     # renders the tag and the strings in the content
+    """Extend the Element.render() method so that it can render other elements inside the tag in addition to strings. Simple recursion should do it. i.e. it can call the render() method of the elements it contains. You’ll need to be smart about setting the ind optional parameter so that the nested elements get indented correctly
+            """
+
     def render(self, file_out, ind=""):
-        self.ind = ind
-        self.indent = 1  # subclass to override class indent (?)
 
-        # content will need to be read in one line at a time and rendered
+        self.file_out = file_out.write(u'{indent}<{tag}>\n'
+            .format(indent=self.indent, tag=self.tag))
 
-        self.file_out = file_out.write(u'{indent}{tag}\n'
-            .format(indent=Element.indent*self.ind, tag=Element.tag[0]))
+        # for c in self.content:  # render each line from list
+        self.file_out = file_out.write(u'{indent}{content}\n'
+            .format(indent=self.indent, content=self.render(self.content)))
 
-        for line in self.content:
-            self.file_out = file_out.write(u'{indent}{content}\n'
-                .format(indent=self.indent*self.ind, content=line))
+        self.file_out = file_out.write(u'{indent}</{tag}>\n'
+            .format(indent=self.indent, tag=self.tag))
 
-        self.file_out = file_out.write(u'{indent}{tag}\n'
-            .format(indent=Element.indent*self.ind, tag=Element.tag[1]))
+# Create subclasses for body and paragraph tags
+class Body(Element):
+    tag = u'body'
+
+class Html(Element):
+    tag = u'!Doctype: HTML'
+
+class P(Element):
+    tag = u'p'
