4abea96afd0c88d754cd6c7274e355db3fc07649
Ian M. Davis <ian.mack.davis@gmail.com>
2014-11-16 22:45:26 -0800

    This commit of the html_render.py script is able to do each of the 8 sections.  Also committing some tests using pytest which tests many of the attributes and methods of each class.

--- Students/imdavis/session06/html_render.py
+++ Students/imdavis/session06/html_render.py
@@ -8,7 +8,7 @@ class Element(object):
     indent_spaces = "    "
     tag = "html"
     attrib = ""
-    def __init__(self, content=None, **kwargs):
+    def __init__(self, content=None, **attributes):
         """
         If no content is passed in at initialization of the object, 
         initialize an empty list otherwise put the content passed in 
@@ -18,33 +18,35 @@ class Element(object):
             self.content = []
         else:
             self.content = [content]
-        # Handle other arguments passed in at initialization
-        if (kwargs):
-            for k, v in kwargs.items():
-                self.attrib += " " + k + "=" + "\"" + v + "\""
+        self.attributes = attributes
+
     def append(self, new_content):
         # append the new content to the list
         if new_content:
             self.content.append(new_content)
+
+    def render_tag(self, current_ind):
+        attribs = "".join([' {}="{}"'.format(k, v) for k, v in self.attributes.items()])
+        tag_str = "{}<{}{}>".format(current_ind, self.tag, attribs)
+        return tag_str
+
     def render(self, file_out, ind=""):
         """
         Be able to handle a string or an 'Element' object as and element
         of the self.content list
         """
-        start_tag = "<"  + self.tag + self.attrib + ">" + "\n"
-        end_tag   = "</" + self.tag + ">" + "\n"
-        file_out.write(ind + start_tag)
+        file_out.write(self.render_tag(ind))
+        file_out.write("\n")
         # This could be a string or an Element object
         for obj in self.content:
             # if a string is the content, handle it this way
             try:
-                writeline = "    " + ind + obj + "\n"
-                file_out.write(writeline)
+                file_out.write( ind + self.indent_spaces + obj + "\n" )
             # if an "Element" object is the content, handle this way
             # keeping track of indentation
             except (TypeError):
-                obj.render(file_out, ind=(self.indent_spaces + ind) )
-        file_out.write(ind + end_tag)
+                obj.render(file_out, self.indent_spaces + ind )
+        file_out.write("{}</{}>\n".format(ind, self.tag))
 
 class Html(Element):
     tag = "html"
@@ -53,7 +55,7 @@ class Html(Element):
         Extension of the render method inherited for this subclass to 
         write the DOCTYPE at the top of the page
         """
-        file_out.write("<!DOCTYPE " + self.tag + ">" + "\n") 
+        file_out.write("<!DOCTYPE {}>\n".format(self.tag))
         Element.render(self, file_out, ind="")
 
 class Body(Element):
@@ -65,14 +67,61 @@ class P(Element):
 class Head(Element):
     tag = "head"
 
-class Title(Element):
-    tag = "title"
+class OneLineTag(Element):
     def render(self, file_out, ind=""):
         """
-        Override the render class we inherit from 'Element' to write the 
+        Extend the render method we inherit from 'Element' to write the 
         title content and tags in a single line.
         """
-        start_tag = ind + "<"  + self.tag + ">"
-        end_tag   = "</" + self.tag + ">"
-        content = "".join(self.content)
-        file_out.write(start_tag + content + end_tag + "\n")
+        file_out.write(self.render_tag(ind))
+        # print the contents of this object on the same line within the tag
+        for obj in self.content:
+            file_out.write(obj)
+        file_out.write("</{}>\n".format(self.tag))
+
+class Title(OneLineTag):
+    tag = "title"
+
+class SelfClosingTag(Element):
+    def render(self, file_out, ind=""):
+        """
+        Extend the render method to render just the one tag and
+        attributes, if any.
+        """
+        file_out.write(ind + "<{} />\n".format(self.tag))
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+
+class Br(SelfClosingTag):
+    tag = "br"
+
+class A(OneLineTag):
+    tag = "a"
+    def __init__(self, link, content=None, **attributes):
+        OneLineTag.__init__(self, content, **attributes)
+        self.attributes["href"] = link
+
+class H(OneLineTag):
+    def __init__(self, level, content=None, **attributes):
+        OneLineTag.__init__(self, content, **attributes)
+        self.tag = "h{}".format(level)
+
+class Ul(Element):
+    tag = "ul"
+
+class Li(Element):
+    tag = "li"
+
+class Meta(SelfClosingTag):
+    tag = "meta"
+    def render(self, file_out, ind=""):
+        """
+        Override the SelfClosingTag render method we inherit to write the 
+        content and tags in a single line with self closing tag
+        """
+        # Slice tag returned by 'render_tag' to remove the trailing '>'
+        # after the tag contents
+        file_out.write(self.render_tag(ind)[:-1])
+        # Write the self closing tag with space
+        file_out.write(" />\n")
--- Students/imdavis/session06/run_html_render.py
+++ Students/imdavis/session06/run_html_render.py
@@ -38,50 +38,50 @@ def render(page, filename):
 ## Step 1
 ##########
 
-page = hr.Element()
+# page = hr.Element()
 
-page.append("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text")
+# page.append("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text")
 
-page.append("And here is another piece of text -- you should be able to add any number")
+# page.append("And here is another piece of text -- you should be able to add any number")
 
-render(page, "test_html_output1.html")
+# render(page, "test_html_output1.html")
 
-# ## Step 2
-# ##########
+# # ## Step 2
+# # ##########
 
-page = hr.Html()
+# page = hr.Html()
 
-body = hr.Body()
+# body = hr.Body()
 
-body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
 
-body.append(hr.P("And here is another piece of text -- you should be able to add any number"))
+# body.append(hr.P("And here is another piece of text -- you should be able to add any number"))
 
-page.append(body)
+# page.append(body)
 
-render(page, "test_html_output2.html")
+# render(page, "test_html_output2.html")
 
-# # Step 3
-# ##########
+# # # Step 3
+# # ##########
 
-page = hr.Html()
+# page = hr.Html()
 
-head = hr.Head()
-head.append(hr.Title("PythonClass = Revision 1087:"))
+# head = hr.Head()
+# head.append(hr.Title("PythonClass = Revision 1087:"))
 
-page.append(head)
+# page.append(head)
 
-body = hr.Body()
+# body = hr.Body()
 
-body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
-body.append(hr.P("And here is another piece of text -- you should be able to add any number"))
+# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
+# body.append(hr.P("And here is another piece of text -- you should be able to add any number"))
 
-page.append(body)
+# page.append(body)
 
-render(page, "test_html_output3.html")
+# render(page, "test_html_output3.html")
 
-# # Step 4
-# ##########
+# # # Step 4
+# # ##########
 
 # page = hr.Html()
 
@@ -99,8 +99,8 @@ render(page, "test_html_output3.html")
 
 # render(page, "test_html_output4.html")
 
-# # Step 5
-# #########
+# # # Step 5
+# # #########
 
 # page = hr.Html()
 
@@ -185,41 +185,41 @@ render(page, "test_html_output3.html")
 # # Step 8
 # ########
 
-# page = hr.Html()
+page = hr.Html()
 
 
-# head = hr.Head()
-# head.append( hr.Meta(charset="UTF-8") )
-# head.append(hr.Title("PythonClass = Revision 1087:"))
+head = hr.Head()
+head.append( hr.Meta(charset="UTF-8") )
+head.append(hr.Title("PythonClass = Revision 1087:"))
 
-# page.append(head)
+page.append(head)
 
-# body = hr.Body()
+body = hr.Body()
 
-# body.append( hr.H(2, "PythonClass - Class 6 example") )
+body.append( hr.H(2, "PythonClass - Class 6 example") )
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
-#               style="text-align: center; font-style: oblique;"))
+body.append(hr.P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+              style="text-align: center; font-style: oblique;"))
 
-# body.append(hr.Hr())
+body.append(hr.Hr())
 
-# list = hr.Ul(id="TheList", style="line-height:200%")
+list = hr.Ul(id="TheList", style="line-height:200%")
 
-# list.append( hr.Li("The first item in a list") )
-# list.append( hr.Li("This is the second item", style="color: red") )
+list.append( hr.Li("The first item in a list") )
+list.append( hr.Li("This is the second item", style="color: red") )
 
-# item = hr.Li()
-# item.append("And this is a ")
-# item.append( hr.A("http://google.com", "link") )
-# item.append("to google")
+item = hr.Li()
+item.append("And this is a ")
+item.append( hr.A("http://google.com", "link") )
+item.append("to google")
 
-# list.append(item)
+list.append(item)
 
-# body.append(list)
+body.append(list)
 
-# page.append(body)
+page.append(body)
 
-# render(page, "test_html_output8.html")
+render(page, "test_html_output8.html")
 
 
 
