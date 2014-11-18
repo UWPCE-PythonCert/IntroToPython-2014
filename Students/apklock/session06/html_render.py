dadf5250865d35b8ab502994ab21c5dfef3cc839
apklock <apklock@gmail.com>
2014-11-18 06:56:38 -0800

    Updates

--- Students/apklock/session06/html_render.py
+++ Students/apklock/session06/html_render.py
@@ -1,4 +1,5 @@
 # html render code...here goes nothing...
+# trying to build using your code but i just have no idea what to do...
 
 class Element(object):
 	tag = 'html'
@@ -13,10 +14,30 @@ class Element(object):
 	def append(self, content): # defining how to append content
 		self.content.append(content)
 		
-	def render_tag(self, current_ind):
+	def render_tag(self, current_ind): # defining the render tag
 		attrbs = "".join(['{} = "{}"'.format(key, val) for key, val in self.attributes.ietms()])
 		tag_str = "{}<{}{}>".format(current_ind, self.tag, attrbs)
 		return tag_str
+	
+	def render(self, file_out, current_ind=""): # defining how the code renders and presents itself
+		file_out.write(self.render_tag(current_ind))
+		file_out.write('\n')
+		for con in self.content:
+			try:
+				file_out.write(current_ind + self.indent + con + "\n")
+			except TypeError:
+				con.render(file_out, current_ind + self.indent)
+		file_out.write("{}</{}>\n".format(current_ind, self.tag))
+	
+class OneLineTag(Element):
+	def render(self, file_out, current_ind=""):
+		file_out.write(self.render_tag(current_ind))
+		for con in self.content:
+			file_out.write(con)
+		file_out.write("{}</{}>\n".format(self.tag))
+		
+class Title(OneLineTag):
+	tag = 'title'
 	
 #def __init__(self, "Python is fun, Python is fast, Python is fascicular but not Macaca fascicularis!!"):
 	#pass
