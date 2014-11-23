dd30e733f2b29c62765a7939a6718b5ef5889330
Ian M. Davis <ian.mack.davis@gmail.com>
2014-11-23 11:33:46 -0800

    The pytest testing module for my Circle class.  Does a bunch of tests.  Circle passes all of these.

--- Students/imdavis/session07/test_circle.py
+++ Students/imdavis/session07/test_circle.py
@@ -60,4 +60,90 @@ def test_from_diameter():
 
 def test_repr():
     c = Circle(6)
-    assert repr(c) == 'Circle(6.0)'
+    assert repr(c) == "Circle(6.0)"
+
+def test_str():
+    c = Circle(4.32)
+    assert c.__str__() == "Circle with radius 4.32"
+    assert str(c) == "Circle with radius 4.32"
+
+def test_add_circle():
+    c1 = Circle(14.34)
+    c2 = Circle(1.15)
+    c3 = c1 + c2
+    assert isinstance(c3, Circle)
+    assert c3.radius == 15.49
+    assert c3.diameter == 30.98
+    c1 += c2
+    assert c1.radius == 15.49
+
+def test_mult_circle():
+    c1 = Circle(2.34)
+    c2 = Circle(5.67)
+    c3 = c1 * c2
+    assert isinstance(c3, Circle)
+    assert c3.radius ==13.2678
+    c3 = c2 * c1
+    assert c3.radius ==13.2678
+    c3 = c1 * 1.2
+    assert isinstance(c3, Circle)
+    assert c3.radius == 2.808
+    c3 = 1.2 * c1
+    assert c3.radius == 2.808
+
+def test_gt():
+    c1 = Circle(2.34)
+    c2 = Circle(5.67)
+    assert c2 > c1
+    assert (c1 > c2) == False
+
+def test_lt():
+    c1 = Circle(2.34)
+    c2 = Circle(5.67)
+    assert c1 < c2
+    assert (c2 < c1) == False
+
+def test_eq():
+    c1 = Circle(2.34)
+    c2 = Circle(5.67)
+    assert (c1 == c2) == False
+    c2 = Circle(2.34)
+    assert c1 == c2
+
+def test_ne():
+    c1 = Circle(2.34)
+    c2 = Circle(5.67)
+    assert c1 != c2
+    assert (c1 == c2) == False
+    c2 = Circle(2.34)
+    assert c1 == c2
+
+def test_nonsense_radius():
+    """
+    Make sure we can't create a Circle object with a negative or zero 
+    radius.
+    """
+    with pytest.raises(AssertionError):
+        c = Circle(-1.234)
+    with pytest.raises(AssertionError):
+        c = Circle(0)
+    c = Circle(1.234)
+    with pytest.raises(AssertionError):
+        c.radius = -1.234 
+    with pytest.raises(AssertionError):
+        c.radius = 0
+
+def test_nonsense_diameter():
+    """
+    Make sure we can't create a Circle object with a negative or zero 
+    diameter.
+    """
+    with pytest.raises(AssertionError):
+        c = Circle.from_diameter(-2.468)
+    with pytest.raises(AssertionError):
+        c = Circle.from_diameter(0)
+    c = Circle.from_diameter(2.468)
+    with pytest.raises(AssertionError):
+        c.diameter = -2.468
+    with pytest.raises(AssertionError):
+        c.diameter = 0
