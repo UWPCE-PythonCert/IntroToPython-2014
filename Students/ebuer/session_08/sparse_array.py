8f13ee19958ad38461e1bb1ff00d0179c1194ced
Anastomose <anastomose@yahoo.com>
2014-11-25 20:44:35 -0800

    Class updates

---
+++ Students/ebuer/session_08/sparse_array.py
@@ -0,0 +1,26 @@
+"""
+sparse_array.py
+"""
+
+class SparseArray(object):
+
+    def __init__(self, arr):
+        self.arr_dict = {i: j for i, j in enumerate(arr)}
+
+    def __call__():
+        pass
+
+    def __len__():
+        pass
+
+    def __getitem__(self, x):
+        return self.arr_dict[x]
+
+    def __setitem__():
+        pass
+
+    def __delitem__():
+        pass
+
+    def __contains__():
+        pass
