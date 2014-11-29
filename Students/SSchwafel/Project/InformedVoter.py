87c8460fbb88ff7a305f37f8ece81c0a7e5c26ac
saschwafel <mr.orange228@gmail.com>
2014-11-28 18:24:04 -0800

    Starting on 'Informed Voter' Project, getting a .json return value, still need to fix the return output and print in a friendly format

---
+++ Students/SSchwafel/Project/InformedVoter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import urllib2
+import simplejson as json
+
+##url = urlopen('https://www.govtrack.us/api/v2/vote?created__gt=2012-01-01T00:00:00')
+url = 'https://www.govtrack.us/api/v2/bill'
+
+# open the url and the screen name 
+# (The screen name is the screen name of the user for whom to return results for)
+#url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=python"
+
+# this takes a python object and dumps it to a string which is a JSON
+# representation of that object
+data = json.load(urllib2.urlopen(url))
+print type(data)
+
+# print the result
+#print data
+
+for key in data.iteritems():
+    print "{}  :  {}".format(key,data[key])
