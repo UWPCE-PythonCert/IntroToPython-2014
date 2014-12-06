71b5b78ed173b4d6209a68f3c3b14e8c96f842dd
Anastomose <anastomose@yahoo.com>
2014-12-06 14:19:04 -0800

    <Minor change to cap_script.py

--- Students/ebuer/session_09/class_examples/capitalize/scripts/cap_script.py
+++ Students/ebuer/session_09/class_examples/capitalize/scripts/cap_script.py
@@ -16,10 +16,9 @@ if __name__ == "__main__":
 
     root, ext = os.path.splitext(infilename)
     outfilename = root + "_cap" + ext
-    
+
     # do the real work:
     print "Capitalizing: %s and storing it in %s"%(infilename, outfilename)
     capital_mod.capitalize(infilename, outfilename)
-    
+
     print "I'm done"
-    
