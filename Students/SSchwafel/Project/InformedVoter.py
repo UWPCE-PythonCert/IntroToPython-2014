d19bde98af6921367fc7fd024e820a27355c7ef9
Schuyler Schwafel <saschwafel@gmail.com>
2014-12-06 14:07:11 -0800

    Splitting project between APIs, fixed title/gender printing

--- Students/SSchwafel/Project/InformedVoter.py
+++ Students/SSchwafel/Project/InformedVoter.py
@@ -4,44 +4,25 @@ from pprint import pprint
 import urllib2
 import simplejson as json
 
-#Bills
-#url = 'https://www.govtrack.us/api/v2/bill'
+user_local_data = json.load(urllib2.urlopen('http://freegeoip.net/json/'))
 
+user_lat = user_local_data['latitude']
+user_long = user_local_data['longitude']
 
-#List Current Members of Congress
-congresspeople_url = 'https://www.govtrack.us/api/v2/role?current=true&limit=600'
+#print gathered lat/long
+#print user_lat,user_long
 
-#One Particular Congressman
-#url = 'https://www.govtrack.us/api/v2/person/400054'
-# this takes a python object and dumps it to a string which is a JSON
-# representation of that object
-data = json.load(urllib2.urlopen(congresspeople_url))
-
-objects = data['objects']
-#for representative in objects:
-#    print representative['person']['name'].encode('utf-8')
-#pprint(objects[0][person]['sortname'])
-
-representatives = []
-
-for i in objects:
-    representatives.append(i['person']['sortname'].encode('utf-8'))
-
-#representatives = sorted(representatives)
-#pprint(representatives)
-
-
-print """
-
-You can get your latitude and longitude from http://www.latlong.net/
-
-"""
+#print """
+#
+#You can get your latitude and longitude from http://www.latlong.net/
+#
+#"""
 
 #user_lat = raw_input('Please enter your Latitude: \n') 
 #user_long = raw_input('Please enter your Longitude: \n')
 
-user_lat = '47.653098'
-user_long = '-122.353731'
+#user_lat = '47.653098'
+#user_long = '-122.353731'
 
 lat_long_url = 'https://congress.api.sunlightfoundation.com/districts/locate?latitude={}&longitude={}&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)
 
@@ -50,9 +31,31 @@ congressional_district = json.load(urllib2.urlopen(lat_long_url))
 
 legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators/locate?latitude={}&longitude={}&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)))
 
+
+#All Legislators, irrespective of location
+
+#House only
+#legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators?chamber=house&per_page=all&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)))
+
+#All Legislators
+#legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators?per_page=all&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)))
+
 #pprint(senators['results'])
-print 'Based on the latitude and longitude provided, your United States Congresspeople are: \n'
+#print 'Based on the latitude and longitude provided, your United States Congresspeople are: \n'
+
+
+#Prints Congressman/Congresswoman + First, Last
+
+#for i in legislators['results']:
+#    pprint(i)
 for i in legislators['results']:
-    first = i['first_name']
-    last = i['last_name']
-    print '{} {}\n'.format(first,last)
+    if i['chamber'] == 'house' and i['gender'] == 'M':
+        print 'Congressman ' + i['first_name'] + ' ' + i ['last_name'] + '\n'
+    elif i['chamber'] == 'senate':
+        print 'Senator ' + i['first_name'] + ' ' + i ['last_name'] + '\n'
+
+
+#for i in legislators['results']:
+#    first = i['first_name']
+#    last = i['last_name']
+#    print '{} {}\n'.format(first,last)
