b36a2e240aabe466367547fae67241cb1f74e510
Salim Hamed <salimhamed@gmail.com>
2014-12-09 17:50:54 -0800

    Finished Session 09 Homework.

--- Students/salim/session09/decorator_lab.py
+++ Students/salim/session09/decorator_lab.py
@@ -2,30 +2,13 @@
 
 
 def p_wrapper(func):
-    def wrap(string, tag):
-        return '<{}>{}</{}>'.format(tag, string.strip(), tag)
+    def wrap(*args, **kwargs):
+        tag = kwargs.get('tag', 'p')  # default tag value is <p>
+        return '<{}>{}</{}>'.format(tag, args[0].strip(), tag)
     return wrap
 
 @p_wrapper
-def return_a_string():
-    return
+def return_a_string(string):
+    return string
 
-return_a_string('this is my string', 'p')
-
-
-# class example
-def logged_func(func):
-    def logged(*args, **kwargs):
-        print "Function %r called" % func.__name__
-        if args:
-            print "\twith args: %r" % args
-        if kwargs:
-            print "\twith kwargs: %r" % kwargs
-        result = func(*args, **kwargs)
-        print "\t Result --> %r" % result
-        return result
-    return logged
-
-@logged_func
-def add(a, b):
-    return a + b
+return_a_string('this is my string')
