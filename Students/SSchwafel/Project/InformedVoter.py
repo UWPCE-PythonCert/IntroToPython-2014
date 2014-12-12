8869c1a9b3e4e1c0677e3c79273fb71ebc0dcad6
saschwafel <mr.orange228@gmail.com>
2014-12-11 21:08:01 -0800

    Final commit of project files.

--- Students/SSchwafel/Project/InformedVoter.py
+++ Students/SSchwafel/Project/InformedVoter.py
@@ -2,71 +2,43 @@
 
 from __future__ import unicode_literals 
 from pprint import pprint
-import urllib2
 import simplejson as json
+import sys
+import urllib2
 
-#user_local_data = json.load(urllib2.urlopen('http://freegeoip.net/json/'))
+#This allows printing to the console, including being able to pipe ( | ) the input
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+#Making sure that there is at least one command line argument, if not, is none
+if len(sys.argv) == 1:
+    command_line_argument = None
+else:
+    command_line_argument = sys.argv[1]
+
+#Finds out the user's IP address for lookup
 user_local_data = json.load(urllib2.urlopen('http://api.ipinfodb.com/v3/ip-city/?key=a648bf3844359d401197bcaa214dd01e0f8c0c6d623ec57f3716fbcafc8262bd&format=json'))
 
+#Set variables for detected lat/long
 user_lat = user_local_data['latitude']
 user_long = user_local_data['longitude']
 
-#print gathered lat/long
-#print user_lat,user_long
+#lat_long_url = 'https://congress.api.sunlightfoundation.com/districts/locate?latitude={}&longitude={}&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)
 
-#print """
-#
-#You can get your latitude and longitude from http://www.latlong.net/
-#
-#"""
-
-#user_lat = raw_input('Please enter your Latitude: \n') 
-#user_long = raw_input('Please enter your Longitude: \n')
-
-#user_lat = '47.653098'
-#user_long = '-122.353731'
-
-lat_long_url = 'https://congress.api.sunlightfoundation.com/districts/locate?latitude={}&longitude={}&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)
-
-congressional_district = json.load(urllib2.urlopen(lat_long_url))
-
-
+#Look up legislators from API with given lat/long, then store in a var. for later use
 legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators/locate?latitude={}&longitude={}&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)))
 
-for i in legislators['results']:
-    print i['last_name'] + ' ' + i['bioguide_id']
 
-#All Legislators, irrespective of location
-
-#House only
-#legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators?chamber=house&per_page=all&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)))
-
-#All Legislators
-#legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators?per_page=all&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)))
-
-#print 'Based on the latitude and longitude provided, your United States Congresspeople are: \n'
-
-
-#Prints Congressman/Congresswoman + First, Last
-
+#Remnant of abandoned functionality
 def find_legislators():
-
+    """This function returns the legislator's last name and unique bioguide id, this would be useful for further, unimplemented features"""
     for i in legislators['results']:
-        print i['last_name'] + ' ' + i['bioguide_id']
-        #legislator_ids.append(i)
+        print '{} {}'.format(i['last_name'], i['bioguide_id'])
 
-#find_legislators()
-
-#votes_url = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/votes?voter_ids.{}__exists=true&apikey=15f4679bdc124cd6a2c6be8666253000')).format( __ THIS IS WHERE THE UNIQUE ID OF THE LEGISLATOR NEEDS TO BE __ )
-
-#pprint(votes_url)
-
-#def recent_votes():
-
-   ##THIS IS WHERE YOU ARE GOING TO RETURN THE LAST 10 VOTES BY var = LEGISLATOR 
-
-def print_legislators():
-
+def print_all_legislators():
+    """This function prints all the legislators, then formats according to gender and title"""
+    
+    legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators?per_page=all&apikey=15f4679bdc124cd6a2c6be8666253000'))
     for i in legislators['results']:
 
         if i['chamber'] == 'house' and i['gender'] == 'M':
@@ -76,4 +48,47 @@ def print_legislators():
         elif i['chamber'] == 'senate':
             print 'Senator {} {} - {} \nPhone: {}\nWebsite: {}\n'.format(i['first_name'],i['last_name'],i['party'],i['phone'],i['website'] )
 
-#print_legislators()
+def print_manual_legislators():
+    """This function prints the legislators of the given lat/long, then formats according to gender and title"""
+    
+    print """
+    
+    You can get your latitude and longitude from http://www.latlong.net/
+    
+    """
+
+    user_lat = raw_input('Please enter your Latitude: \n') 
+    user_long = raw_input('Please enter your Longitude: \n')
+
+    legislators = json.load(urllib2.urlopen('https://congress.api.sunlightfoundation.com/legislators/locate?latitude={}&longitude={}&apikey=15f4679bdc124cd6a2c6be8666253000'.format(user_lat, user_long)))
+    
+    for i in legislators['results']:
+
+        if i['chamber'] == 'house' and i['gender'] == 'M':
+            print 'Congressman {} {} - {} \nPhone: {}\nWebsite: {}\n'.format(i['first_name'],i['last_name'],i['party'],i['phone'],i['website'] )
+        if i['chamber'] == 'house' and i['gender'] == 'F':
+            print 'Congresswoman {} {} - {} \nPhone: {}\nWebsite: {}\n'.format(i['first_name'],i['last_name'],i['party'],i['phone'],i['website'] )
+        elif i['chamber'] == 'senate':
+            print 'Senator {} {} - {} \nPhone: {}\nWebsite: {}\n'.format(i['first_name'],i['last_name'],i['party'],i['phone'],i['website'] )
+
+def print_detected_legislators():
+    """This function prints the legislators local to the IP address of the user, then formats according to gender and title"""
+    for i in legislators['results']:
+
+        if i['chamber'] == 'house' and i['gender'] == 'M':
+            print 'Congressman {} {} - {} \nPhone: {}\nWebsite: {}\n'.format(i['first_name'],i['last_name'],i['party'],i['phone'],i['website'] )
+        if i['chamber'] == 'house' and i['gender'] == 'F':
+            print 'Congresswoman {} {} - {} \nPhone: {}\nWebsite: {}\n'.format(i['first_name'],i['last_name'],i['party'],i['phone'],i['website'] )
+        elif i['chamber'] == 'senate':
+            print 'Senator {} {} - {} \nPhone: {}\nWebsite: {}\n'.format(i['first_name'],i['last_name'],i['party'],i['phone'],i['website'] )
+
+if command_line_argument == '--all-legislators':
+    print_all_legislators()
+
+elif command_line_argument == None:
+    print 'Based on the latitude and longitude detected, your United States Congresspeople are: \n'
+    print_detected_legislators()
+
+elif command_line_argument == '--manual':
+    print_manual_legislators()
+
