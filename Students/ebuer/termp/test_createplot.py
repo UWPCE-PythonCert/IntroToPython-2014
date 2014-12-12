cf80b30e5d5b401eadec74120f2061f09061d2bf
Anastomose <anastomose@yahoo.com>
2014-12-11 17:11:30 -0800

    Adding some new tests

---
+++ Students/ebuer/termp/test_createplot.py
@@ -0,0 +1,32 @@
+
+import sys
+sys.path.append(r"D:\GitHub\IntroToPython\Students\ebuer\termp")
+
+from termp_funcs import plotprep
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from createplot import callclass, plotSampdict
+
+def tsetup():
+    chem = callclass()
+    chem.selectSample('14051202')
+    chem.selectForPlot('SW8082A')
+    plot_dict = chem.makePlotobj()
+    return (chem, plot_dict)
+
+
+def test_pSd():
+    chem, plot_dict = tsetup()
+    fig1, ax1 = plotSampdict(**plot_dict)
+
+    assert type(fig1) is plt.Figure
+    assert type(ax1) is mpl.axes.Subplot
+
+def test_ax1():
+    chem, plot_dict = tsetup()
+    fig1, ax1 = plotSampdict(**plot_dict)
+
+    assert ax1.xaxis.get_label_text() == 'Sample Parameter'
