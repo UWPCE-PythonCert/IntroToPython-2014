e3d70a4021193afc77c0c27862e3afe59dca8bb2
Bryan Davis <bryan@neoty.pe>
2014-12-12 00:28:29 -0800

    another update

---
+++ Students/brydavis/project/really/test1418371830.sql
@@ -0,0 +1,1 @@
+CREATE TABLE table_one (guid TEXT,index INTEGER,favoriteFruit TEXT,latitude FLOAT,company TEXT,email TEXT,picture TEXT,registered TEXT,eyeColor TEXT,phone TEXT,address TEXT,isActive BOOLEAN,about TEXT,_id TEXT,name TEXT,gender TEXT,age INTEGER,greeting TEXT,longitude FLOAT,balance unicode);CREATE TABLE table_one (id INTEGER,name unicode);
