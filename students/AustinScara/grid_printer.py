bab755151f93a1a5a24c663a46d24956e6a5f826
austinscara <austin.scara@gmail.com>
2015-10-13 23:29:32 -0700

    gridly

--- students/AustinScara/grid_printer.py
+++ students/AustinScara/grid_printer.py
@@ -1,10 +1,10 @@
 def grid(n):
 	headMidBot = "+ " + "- "*n + "+" + " -"* n+ " +"
-	mid = "|" + "  "*n + " + " + "  "*n + "|" + '\n'
+	mid = "|" + "  "*n + " + " + "  " *n + "|" + '\n'
 	return headMidBot +'\n' + mid*n + headMidBot + '\n' + mid*n + headMidBot
 
 
 
+size = int(input("How big Do you want this grid to be?"))
 
-
-print (grid(10))
+print (grid(size))
