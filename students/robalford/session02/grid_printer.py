072c308570126ef0e445264f062e04c46b1bee61
robalford <robertsalford@gmail.com>
2015-10-14 12:40:27 -0700

    first version

---
+++ students/robalford/session02/grid_printer.py
@@ -0,0 +1,12 @@
+def print_grid():
+    # return value, then print function
+    top_row = ('+ ' + '- ' * 4) * 2 + '+ \n'
+    next_four = ('|' + ' ' * 9) * 2 + '| \n'
+    return (top_row + next_four * 4) * 2 + top_row 
+    
+
+    
+    
+    
+    
+    
