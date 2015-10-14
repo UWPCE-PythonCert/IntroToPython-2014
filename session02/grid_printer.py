57fc3934ce1965ca4c71e30dc958843a32f99630
Eric Rosko <eric_rosko@hotmail.com>
2015-10-13 20:15:01 -0700

    Added first version of grid printout

---
+++ session02/grid_printer.py
@@ -0,0 +1,18 @@
+
+#def print topedge():
+#	print("+" + (" -" * 4))
+
+toppart = "+ " + ("- " * 4)
+plus = "+"
+middlepart = "| " + (" " * 8)
+pipe = "|"
+middleline = middlepart * 2 + pipe + "\n"
+
+
+print(toppart * 2 + plus)
+print(middleline * 4, end='')
+
+print(toppart * 2 + plus)
+print(middleline * 4, end='')
+
+print(toppart * 2 + plus)
