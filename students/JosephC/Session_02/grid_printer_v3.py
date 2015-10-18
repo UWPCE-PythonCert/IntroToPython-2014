cc60df0fb2709b314d414699d730f0258517e3d9
Joseph Cardenas <cardej2@u.washington.edu>
2015-10-18 11:40:25 -0700

    Committing new files

---
+++ students/JosephC/Session_02/grid_printer_v3.py
@@ -0,0 +1,33 @@
+# grid printer
+
+def printer():
+    print('+', '-'*4, '+', '-'*4, '+')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('+', '-'*4, '+', '-'*4, '+')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('|', ' '*4, '|', ' '*4, '|')
+    print('+', '-'*4, '+', '-'*4, '+')
+#the above is the stupid way
+
+printer()
+
+#create a line to differentiate the boxes
+print("----------------------")
+
+#use the arugment n to define the size of the grid
+#as the problem is underspecified, n shall blow up the spaces in the grid, rather than add more columns
+def print_grid(n):
+   
+
+print_grid(4)
+
+#create a function that allows the user to define the number of columns and rows
+def column_adder(x,y):
+    """x controls the number of columns, y controls the number of rows."""
+    print()
+
