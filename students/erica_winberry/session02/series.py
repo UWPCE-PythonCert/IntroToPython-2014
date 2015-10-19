54afa936db266d23aca5c3637d14bace6dbc0a92
Erica Winberry <elaewin@gmail.com>
2015-10-19 13:51:34 -0700

    Creating the files for week 2, lab 3 (fibonacci series/lucas series)

---
+++ students/erica_winberry/session02/series.py
@@ -0,0 +1,11 @@
+# Create a new module series.py in the session02 folder in your student folder.
+#     In it, add a function called fibonacci.
+#     The function should have one parameter n.
+#     The function should return the nth value in the fibonacci series.
+#     Ensure that your function has a well-formed docstring
+# Note that the fibinacci series is naturally recusive – the value is defined by previous values:
+#     fib(n) = fib(n-2) + fib(n-1)
+
+def fibonacci(n):
+    """Return the nth value in the fibonacci series."""
+    
