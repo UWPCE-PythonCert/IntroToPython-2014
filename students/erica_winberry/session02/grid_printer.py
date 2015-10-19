e7869034a60c43410db72155eddd97bcd2abf430
Erica Winberry <elaewin@gmail.com>
2015-10-19 16:45:04 -0700

    No futher changes made to the code.

--- students/erica_winberry/session02/grid_printer.py
+++ students/erica_winberry/session02/grid_printer.py
@@ -1,8 +1,8 @@
-"""def grid_printer():
+'''def grid_printer():
     top = ('+' + ('-' * 4)) * 2 + '+\n'
     side = ('|' + (' ' * 4)) * 2 + '|\n'
     return top + side * 4 + top + side * 4 + top
-print(grid_printer())"""
+print(grid_printer())'''
 
 def grid_printer(x,y,z): # x is width, y is height, z is number of columns
     top = ("+" + ("-" * x)) * z + "+\n"
