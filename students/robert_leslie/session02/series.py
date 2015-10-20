79d3cd1d21220511c959c86f3d03c350c37664f1
Robert Leslie <frenchforthe@gmail.com>
2015-10-19 21:42:16 -0700

    fixed functions to add 'return' and fixed assertions in serial.py

--- students/robert_leslie/session02/series.py
+++ students/robert_leslie/session02/series.py
@@ -12,18 +12,21 @@ def fibonacci(n):
     fib(n) = fib(n-2) + fib(n-1)
     """
     if n == 1:
-        print('0')
+        print(0)
+        return 0
     elif n == 2:
-        print('1')
+        print(1)
+        return 1
     else:
         f = 0
         s = 1
         for i in range(2, n):
             p = f + s
-            if i == n-1:
-                print(p)
             s = p
             f = p - f
+            if i == n-1:
+                print(p)
+                return p
         
     
 
@@ -36,19 +39,21 @@ def lucas(n):
     luc(n) = luc(n-2) + luc(n-1)
     """   
     if n == 1:
-        print('2')
+        print(2)
+        return 2
     elif n == 2:
-        print('1')
+        print(1)
+        return 1
     else:
         f = 2
         s = 1
         for i in range(2, n):
             p = f + s
-            if i == n-1:
-                print(p)
             s = p
             f = p - f
-
+            if i == n-1:
+                print(p)
+                return p
     
     
     
@@ -64,40 +69,43 @@ def sum_series(n, f=0, s=1):
     """   
     if n == 1:
         print(f)
+        return f
     elif n == 2:
         print(s)
+        return s
     else:
         for i in range(2, n):
             p = f + s
-            if i == n-1:
-                print(p)
             s = p
             f = p - f
+            if i == n-1:
+                print(p)
+                return p
 
 
 
 
 if __name__ == "__main__":
     # run some tests
-    assert fibonacci(0) == 0
-    assert fibonacci(1) == 1
+    assert fibonacci(1) == 0
     assert fibonacci(2) == 1
-    assert fibonacci(3) == 2
-    assert fibonacci(4) == 3
-    assert fibonacci(5) == 5
-    assert fibonacci(6) == 8
-    assert fibonacci(7) == 13
-
-    assert lucas(0) == 2
-    assert lucas(1) == 1
-
-    assert lucas(4) == 7
-
+    assert fibonacci(3) == 1
+    assert fibonacci(4) == 2
+    assert fibonacci(5) == 3
+    assert fibonacci(6) == 5
+    assert fibonacci(7) == 8
+    assert fibonacci(8) == 13
+    
+    assert lucas(1) == 2
+    assert lucas(2) == 1
+    
+    assert lucas(5) == 7
+    
     assert sum_series(5) == fibonacci(5)
-
+    
     # test if sum_series matched lucas
     assert sum_series(5, 2, 1) == lucas(5)
-
+    
     print("tests passed")
 
 
