00f7a1a92a9c8155f745ee17d07421a41e5a7abc
Max MacCamy <maxmac12@gmail.com>
2015-10-20 17:53:54 -0700

    Updated series.py to make the use of range better.

--- students/maxmaccamy/session02/fizzbuzz.py
+++ students/maxmaccamy/session02/fizzbuzz.py
@@ -1,6 +1,5 @@
 def fizzbuzz():
-    for i in range(100):
-        i += 1
+    for i in range(1,101):
         if ((0 == i % 5) and (0 == i % 3)):
             print("FizzBuzz")
         elif (0 == i % 5):
