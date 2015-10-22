a91d0408d42e8afcbbbd4bc8a7627b907c75464d
robalford <robertsalford@gmail.com>
2015-10-22 13:57:42 -0700

    series 2 of list_lab.py

--- students/robalford/session03/list_lab.py
+++ students/robalford/session03/list_lab.py
@@ -1,3 +1,5 @@
+# Series 1
+
 fruity_list = ['Apples', 'Pears', 'Oranges', 'Peaches']
 
 print(fruity_list)
@@ -10,7 +12,7 @@ print(fruity_list)
 
 number_fruit = input('Give me a number between one and five!')
 
-print(number_fruit + fruity_list[int(number_fruit)-1])
+print(number_fruit + ' ' + fruity_list[int(number_fruit)-1])
 
 fruity_list = ['Grapefruit'] + fruity_list
 
@@ -23,3 +25,29 @@ print(fruity_list)
 for fruit in fruity_list:
     if fruit[0] == 'P':
         print(fruit)
+
+# Series 2
+
+print(fruity_list)
+
+fruity_list.pop()
+
+print(fruity_list)
+
+fruit_gone = input('Give me a fruit to lose!')
+
+fruity_list.remove(fruit_gone)
+
+print(fruity_list)
+
+double_fruity_list = fruity_list * 2
+
+# fruit_gone = input('Give me a fruit to lose!')
+# refactor to make this more pythonic?
+while fruit_gone not in double_fruity_list:
+    fruit_gone = input('Give me a fruit to lose!')
+for fruit in double_fruity_list:
+    if fruit == fruit_gone:
+        double_fruity_list.remove(fruit_gone)
+
+print(double_fruity_list)
