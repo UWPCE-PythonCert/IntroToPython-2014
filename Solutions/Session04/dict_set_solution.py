a842a36b447c1766f50abb96fccd21beac5a3269
Chris Barker <PythonCHB@gmail.com>
2015-10-24 14:08:20 -0700

    updated a couple session 4 solutions to py3 -- not done yet

--- Solutions/Session04/dict_set_solution.py
+++ Solutions/Session04/dict_set_solution.py
@@ -13,16 +13,16 @@ d = {"name": "Chris",
      "cake": "Chocolate"}
 
 # Display the dictionary.
-print d
+print(d)
 
-#or something fancier, like:
-print "{name} is from {city}, and likes {cake} cake.".format(name=d['name'],
-                                                             city=d['city'],
-                                                             cake=d['cake'])
+# or something fancier, like:
+print ("{name} is from {city}, and likes {cake} cake.".format(name=d['name'],
+                                                              city=d['city'],
+                                                              cake=d['cake']))
 
 # But if that seems like unnecceasry typing -- it is:
 # we'll learn the **d form is session05:
-print "{name} is from {city}, and likes {cake} cake.".format(**d)
+print("{name} is from {city}, and likes {cake} cake.".format(**d))
 
 
 # Delete the entry for "cake".
@@ -30,21 +30,21 @@ print "{name} is from {city}, and likes {cake} cake.".format(**d)
 
 del d["cake"]
 
-print d
+print(d)
 
 # Add an entry for "fruit" with "Mango" and display the dictionary.
 
 d['fruit'] = 'Mango'
 
-print d
+print(d)
 
 # Display the dictionary keys.
 
-print d.keys()
+print(d.keys())
 
 # Display the dictionary values.
 
-print d.values()
+print(d.values())
 
 # Display whether or not "cake" is a key in the dictionary (i.e. False) (now).
 
@@ -77,22 +77,14 @@ print a_dict
 
 
 # replacing the values in the original dict:
-<<<<<<< HEAD
-
 for key, val in d.items():
     d[key] = val.count('t')
-print d
+print(d)
 
-=======
+# Or the direct way -- update() is very handy!
 d.update(a_dict)
-print d
+print(d)
 
-# could also do it this way. 
-#for key, val in d.items():
-#    d[key] = val.count('t')
-#print d
-
->>>>>>> 46ce6ea660d7458ad07d0a092349a82db8fecb7c
 # Create sets s2, s3 and s4 that contain numbers from zero through
 # twenty, divisible 2, 3 and 4.
 
@@ -102,25 +94,23 @@ s2 = set()
 s3 = set()
 s4 = set()
 for i in range(21):
-    if not i%2:
+    if not i % 2:
         s2.add(i)
-    if not i%3:
+    if not i % 3:
         s3.add(i)
-    if not i%4:
+    if not i % 4:
         s4.add(i)
 
-print s2
-print s3
-print s4
+print(s2)
+print(s3)
+print(s4)
 
 
 # Display if s3 is a subset of s2 (False)
-
-print s3.issubset(s2)
+print(s3.issubset(s2))
 
 # and if s4 is a subset of s2 (True).
-
-print s4.issubset(s2)
+print(s4.issubset(s2))
 
 # Create a set with the letters in 'Python' and add 'i' to the set.
 
