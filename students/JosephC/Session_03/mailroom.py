0f1c36b5dcdfc21746ef97411240250174c901e8
Joseph Cardenas <cardej2@u.washington.edu>
2015-10-25 14:09:07 -0700

    updates to mailroom and slicing problems

--- students/JosephC/Session_03/mailroom.py
+++ students/JosephC/Session_03/mailroom.py
@@ -1,8 +1,10 @@
 #Mailroom 
+'''
+We have a list of donors and the amounts they have donated. The user will be prompted to enter a donor name or simply generate a list of current donors.  
+If the name entered is not on the original list, then the user will enter that name and the amount donated, these values then being appended to the main donor list. 
+There is also the option of sending a Thank You email to a particular donor.
 
-
-
-
+'''
 #   define the donor list
 def donor_List():
         
@@ -14,13 +16,11 @@ def donor_List():
 #   a function to print the list of donors
 def report():
     print(donors)
-#   create a 
-def New_Donor():
+#   create a new donor (to be used if a given name is not already on the list of donors)
+def create_New_Donor():
      new_Donor = []
-     new_Name = input("Please enter the name of this new donor."    
+     new_Name = input("Please enter the name of this new donor.")    
      new_Donor.append(new_Name)
-     
-           
      donors.append(new_Donor)
 
 #   create a function to record a new amount
@@ -32,18 +32,18 @@ def New_Amount():
     
 #   create the Thank You email
 def email():
-    if New_Donor():
+    if create_New_Donor():
         
     
 
 #   generate a Thank You for a donor 
 def thank_You():
-        name = input("Enter the full name of the recipient, or enter 'list' if you would like to review our list: ")
-        if name == 'list' or 'List':
-            report()
-        #   append to the donors list if the name is not there by creating a new list and appending it to the original donors list  
-        elif name not in donors:
-           New_Donor()       
+    name = input("Enter the full name of the recipient, or enter 'list' if you would like to review our list: ")
+    if name == 'list' or 'List':
+        report()
+    #   append to the donors list if the name is not there by creating a new list and appending it to the original donors list  
+    elif name not in donors:
+        New_Donor()       
 
     
 #   main function 
