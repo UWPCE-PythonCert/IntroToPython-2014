fba1a45d2b7b1226a384866fe5e4026f526d43f4
Erica Winberry <elaewin@gmail.com>
2015-10-24 17:40:45 -0700

    Created new file for the ROT13 portion of the homework.

---
+++ students/erica_winberry/session03/rot13.py
@@ -0,0 +1,14 @@
+# This module should provide at least one function called rot13 that takes 
+# any amount of text and returns that same text encrypted by ROT13.
+
+# This function should preserve whitespace, punctuation and capitalization.
+
+# Your module should include an if __name__ == '__main__': block with tests (asserts) 
+# that demonstrate that your rot13 function and any helper functions you add work properly.
+
+# There is a “short-cut” available that will help you accomplish this task.
+# Some spelunking in the documentation for strings should help you to find it.
+# If you do find it, using it is completely fair game.
+
+# As usual, add your new file to your local clone right away. 
+# Make commits early and often and include commit messages that are descriptive and concise.
