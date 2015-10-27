21c752c2926e3081fd0d00e26d23bc295b064584
robalford <robertsalford@gmail.com>
2015-10-27 11:43:48 -0700

    main program working in mailroom.py. need to fix bug for multiple create_report calls

--- students/robalford/session03/mailroom.py
+++ students/robalford/session03/mailroom.py
@@ -1,3 +1,5 @@
+import sys
+
 donors = [
     ['John Lennon', 100.00, 50.00, 586.78],
     ['Paul McCartney', 200.00, 150.00],
@@ -13,6 +15,12 @@ Enter 'Thank you' to send a thank you message.
 Enter 'Report' to create a report.
 Enter 'Home' to return to this screen.
 Enter 'Quit' to exit My Donation Manager.""")
+    while command.lower() == 'thank you':
+        command = write_email()
+    while command.lower() == 'report':
+        command = create_report()
+    while command.lower() == 'quit':
+        command = sys.exit()
     return command
 
 
@@ -20,8 +28,6 @@ def prompt_user(prompt):
     command = input(prompt)
     while command.lower() == 'home':
         command = select_command()
-    while command.lower() == 'quit':
-        return
     return command
 
 
@@ -60,6 +66,7 @@ def write_email():
     Your favorite charity """.format(full_name, donation_amount)
 
     print(thank_you)
+    return select_command()
 
 
 def create_report():
@@ -71,16 +78,30 @@ def create_report():
         number_of_donations = len(donor[1:])
         average_donation = total_donation/number_of_donations
         print('{}\t\t{:.2f}\t\t{:d}\t\t{:.2f}'.format(donor[0], total_donation, number_of_donations, average_donation))
+    return select_command()
 
 if __name__ == '__main__':
-    # this isnt working ... cant go from report back to thank you
     command = select_command()
-    while command.lower() == 'thank you':
-        write_email()
-        command = select_command()
-    while command.lower() == 'report':
-        create_report()
-        command = select_command()
+    # while command == 'thank you':
+    #     command = write_email()
+    # while command == 'report':
+    #     command = create_report()
+
+
+
+
+    # command = select_command()
+    # # while command == select_command():
+    # while command.lower() == 'thank you':
+    #     write_email()
+    #     command = select_command()
+    #     break
+    # # command = select_command()
+    # while command.lower() == 'report':
+    #     create_report()
+    #     command = select_command()
+    #     break
+
 
 
 
