90f05c7a4dbfb3eb468b284529f31a09ea206395
robalford <robertsalford@gmail.com>
2015-10-27 12:49:36 -0700

    fixed bug in write_email()

--- students/robalford/session03/mailroom.py
+++ students/robalford/session03/mailroom.py
@@ -9,6 +9,13 @@ donors = [
 ]
 
 
+def get_donor_list():
+    donor_list = []
+    for donor in donors:
+        donor_list.append(donor[0])
+    return donor_list
+
+
 def select_command():
     command = input("""Commands:
 Enter 'Thank you' to send a thank you message.
@@ -40,10 +47,6 @@ def write_email():
         for donor in donors:
             print(donor[0])
         full_name = prompt_user("Enter the donor's full name or type 'list' to see all donors.")
-        break
-
-    if full_name not in donors:
-        donors.append([full_name])
 
     donation_amount = prompt_user('Enter the donation amount.')
 
@@ -54,15 +57,22 @@ def write_email():
         except:
             donation_amount = prompt_user('Please enter a numeric value.')
 
+    new_donor = True
     for donor in donors:
         if full_name == donor[0]:
+            new_donor = False
             donor.append(donation_amount)
 
+    if new_donor:
+        donors.append([full_name, donation_amount])
+
     thank_you = """Dear {},
+
     Thank you for your generous donation of ${}. You're
     the best!
 
     Sincerely,
+
     Your favorite charity """.format(full_name, donation_amount)
 
     print(thank_you)
@@ -82,28 +92,3 @@ def create_report():
 
 if __name__ == '__main__':
     command = select_command()
-    # while command == 'thank you':
-    #     command = write_email()
-    # while command == 'report':
-    #     command = create_report()
-
-
-
-
-    # command = select_command()
-    # # while command == select_command():
-    # while command.lower() == 'thank you':
-    #     write_email()
-    #     command = select_command()
-    #     break
-    # # command = select_command()
-    # while command.lower() == 'report':
-    #     create_report()
-    #     command = select_command()
-    #     break
-
-
-
-
-
-
