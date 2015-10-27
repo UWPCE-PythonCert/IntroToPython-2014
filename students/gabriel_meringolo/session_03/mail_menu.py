0d23ed2f66ffd99ff265c88aacf2f9db70eb16ab
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-10-27 14:44:49 -0700

    Added more functions, but add_donor won't return value...

--- students/gabriel_meringolo/session_03/mail_menu.py
+++ students/gabriel_meringolo/session_03/mail_menu.py
@@ -1,9 +1,12 @@
 donors = [["Graham", 1, 5, 10], ["Eric", 4, 6], ["Terry", 5, 5, 5], ["John", 20], ["Michael", 10, 10]]
+doncopy = donors[:]
+
 
 
 def get_two(item):
     '''returns second item in list'''
     return item[1]
+
 
 
 def report(x): #compiles the donor list descending by total donations
@@ -23,6 +26,7 @@ def report(x): #compiles the donor list descending by total donations
     print("\n")
 
 
+
 def mail_head(x):
     '''
     prints mailroom greeting
@@ -34,7 +38,7 @@ def mail_head(x):
     print("|           V 1.0            |")
     print("#----------------------------#\n\n")
     input("Press enter to continue.\a\n")
-    print("Welcome to the Python Endowment Fund mailroom.\n\n")
+    print("Welcome to the Python Endowment Fund mailroom.")
     mail_menu(x)
 
 
@@ -50,7 +54,7 @@ def mail_menu(x):
     :param x: donor database list
     :return:
     '''
-    print("Please make a selection from the menu below.")
+    print("\nPlease make a selection from the menu below.")
     print("--------------------------------------------\a")
     menu = input("1: Send a 'Thank You' Letter to Donor\n2: Create Donor report\n3: Quit\n>")
     if menu == "1":
@@ -64,17 +68,126 @@ def mail_menu(x):
 
 
 def thank_you(x):
+    '''
+    displays thank you letter menu
+    :param x: donor database list
+    :return:
+    '''
     pers = input("\nPlease make a selection from the menu below\n"
                  "-------------------------------------------\n"
                  "1. Enter Donor name\n2. Display list of Donors\n3. Return to Main Menu\n4. Quit\n> ")
     if pers == "1":
-        donor_check(x)
+        add_donate(x, (donor_check(x)))
     if pers == "2":
         donor_names(x)
         thank_you(x)
     if pers == "3":
-        mail_menu()
-    if menu.lower() == "quit" or menu == "4":
+        mail_menu(x)
+    if pers.lower() == "quit" or pers == "4":
         quit_mr()
 
-mail_head(donors)
+
+def flat_list(x):
+    '''
+    flattens nested list and makes new list
+    :param x: nested list
+    :return:
+    '''
+    flat = []
+    for i in x:
+        for ii in i:
+            flat.append(ii)
+    return flat
+
+
+def donor_names(x):
+    '''
+    prints list of donor names
+    :param x: donor list
+    :return: donors ini x
+    '''
+    for i in x:
+        print(i[0])
+    print("\n")
+
+def add_donor(x, dude):
+    '''
+    adds donor to list
+    :param x: donor database list
+    :param y: donor name
+    :return: donor name y added at end of list x
+    '''
+    x.append([dude, ])
+    print("Added Donor: {}".format(dude))
+    print(dude)
+    return dude
+
+
+def donor_check(x):
+    '''
+    checks for donor on list
+    :param x: donor database list
+    :param y: donor name
+    :return:
+    '''
+    #new_don = ""
+    y = input("\nEnter Donor name\n> ")
+    if y.lower() == "quit":
+        quit_mr()
+    if y in flat_list(x):
+        print("\nDonor Found")
+        return y
+    elif y not in flat_list(x):
+        #print(new_don, "rad")
+        #new_don = y
+        #print(new_don, "radder")
+        add_donor(x,y)
+
+
+def add_donate(x, y):
+    '''
+    adds a donation
+    :param x: donor database list
+    :param y: donor name
+    :return:
+    '''
+    print(x)
+    print(y)
+    z = input("\nEnter donation amount\n> ")
+    if num_check(x, y, z) == True:
+        print("true")
+        print(y)
+        #for i in x:
+         #   print(y)
+         #   print(i[0])
+            #print(y)
+            #print(x[0])
+            #if i[0] == y:
+                #print(i)
+                #i.append(z)
+                ##print(i)
+                #print(x) # prints updated liast
+
+
+
+
+def num_check(x, y, z):
+    try:
+        float(z) or int(z)
+        return True
+    except:
+        print("That is not a valid donation")
+        add_donate(x, y)
+
+
+def iso_donor(x, y):
+    for i in x:
+        if y in i:
+            return i
+
+
+"""x = donor list
+   y = donor name
+   z = donation amount
+"""
+mail_head(doncopy)
