bad8900baa347318eef24dbe0d6e4588dc468e02
Erica Winberry <elaewin@gmail.com>
2015-10-27 18:56:24 -0700

    Created the new file and completed the first two steps: creating the dictionary and printing it.

---
+++ students/erica_winberry/session04/dict_lab.py
@@ -0,0 +1,11 @@
+# Create a dictionary containing “name”, “city”, and “cake” for “Chris” from “Seattle” who likes “Chocolate”.
+d = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
+print(d)
+# Display the dictionary.
+# Delete the entry for “cake”.
+# Display the dictionary.
+# Add an entry for “fruit” with “Mango” and display the dictionary.
+# Display the dictionary keys.
+# Display the dictionary values.
+# Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
+# Display whether or not “Mango” is a value in the dictionary (i.e. True).
