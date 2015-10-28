bc8e8c5effe8baf102d6315f8e69eb787e150724
Erica Winberry <elaewin@gmail.com>
2015-10-27 19:09:17 -0700

    Completed part one of the dictionary lab.

--- students/erica_winberry/session04/dict_lab.py
+++ students/erica_winberry/session04/dict_lab.py
@@ -1,11 +1,19 @@
 # Create a dictionary containing “name”, “city”, and “cake” for “Chris” from “Seattle” who likes “Chocolate”.
 d = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
+# Display the dictionary.
 print(d)
-# Display the dictionary.
 # Delete the entry for “cake”.
+d.pop("cake")
 # Display the dictionary.
+print(d)
 # Add an entry for “fruit” with “Mango” and display the dictionary.
+d.update({"fruit": "Mango"})
 # Display the dictionary keys.
+print(d.keys())
 # Display the dictionary values.
+print(d.values())
 # Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
+print(d.get("cake", "Not there!"))
 # Display whether or not “Mango” is a value in the dictionary (i.e. True).
+if "Mango" in d.values():
+    print("True")
