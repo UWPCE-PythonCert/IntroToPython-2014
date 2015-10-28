2e0b76d767e86d7440c914eedc1bf342db65b247
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-10-27 18:53:56 -0700

    Added dict_lab

---
+++ students/gabriel_meringolo/session_04/dict_lab.py
@@ -0,0 +1,19 @@
+#When the script is run, it should accomplish the following four series of actions:
+
+#Create a dictionary containing “name”, “city”, and “cake” for “Chris” from “Seattle” who likes “Chocolate”.
+#Display the dictionary.
+#Delete the entry for “cake”.
+##Display the dictionary.
+#Add an entry for “fruit” with “Mango” and display the dictionary.
+#Display the dictionary keys.
+#Display the dictionary values.
+#Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
+#Display whether or not “Mango” is a value in the dictionary (i.e. True).
+#Using the dictionary from item 1: Make a dictionary using the same keys but with the number of ‘t’s in each value.
+#Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.
+#Display the sets.
+#Display if s3 is a subset of s2 (False)
+#and if s4 is a subset of s2 (True).
+#Create a set with the letters in ‘Python’ and add ‘i’ to the set.
+#Create a frozenset with the letters in ‘marathon’
+#display the union and intersection of the two sets.
