2431a18642cb13360f81ff1e43b31f55922b772a
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-10-29 16:45:52 -0700

    working

--- students/gabriel_meringolo/session_04/trigram.py
+++ students/gabriel_meringolo/session_04/trigram.py
@@ -1,11 +1,24 @@
-x = "I wish I may I wish I might"
-first_two = ""
-" ".join(x.split()[:2])
-if 
-print(" ".join(x.split()[:2]))
+x = open("sherlock.txt", "r").read().split() # opened txt file as file object makes it readable and splits it
+first_two = " ".join(x[:2])
+print(first_two)
+#x = y.read() # reading file object
+
+#first_two = " ".join(x.split()[:2]) # isolating first two words
+
+#print(first_two)
+#x[x.index(first_two) + 1]
+#print(x[x.find(first_two) + len(first_two) + 1])
+
+#next_word = first_two.split()[1]
+#print(next_word)
+
+#print(first_two.split()[1])
+#print(x[x.find(first_two) + len(first_two) + 1:])
+#print(first_two)
+#if first_two in x:
+#    print(x[x.find(first_two) + len(first_two) + 1:])
+#print(" ".join(x.split()[:2]))
 
 #print(x[x.find("may") + len("may") : ])
-#"wish I" => ["may", "might"]
-#"may I"  => ["wish"]
-#"I may"  => ["I"]
+
 
