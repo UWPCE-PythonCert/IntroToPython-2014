a25ffbfd974a4d140c4486b1140def183f255455
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-10-29 16:16:18 -0700

    started trigramlab

---
+++ students/gabriel_meringolo/session_04/trigram.py
@@ -0,0 +1,11 @@
+x = "I wish I may I wish I might"
+first_two = ""
+" ".join(x.split()[:2])
+if 
+print(" ".join(x.split()[:2]))
+
+#print(x[x.find("may") + len("may") : ])
+#"wish I" => ["may", "might"]
+#"may I"  => ["wish"]
+#"I may"  => ["I"]
+
