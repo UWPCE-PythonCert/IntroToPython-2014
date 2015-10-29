fc37e88f94992fd873b9a379b277a736b2e0f05d
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-10-29 12:14:33 -0700

    cleaning up the and isolating the languages

--- students/gabriel_meringolo/session_04/file_lab.py
+++ students/gabriel_meringolo/session_04/file_lab.py
@@ -1,6 +1,14 @@
 studs = open("students.txt", "r+")
+studs.readline()
 for i in studs:
-    print(i[i.find(":") + 1:], end = "")
+    cleani = i.strip()
+    #print(cleani)
+    if cleani:
+        print(cleani[cleani.find(":") + 1:], end = "")
+    #print(i[i.find(":") + 1:].strip("\n"))
+    #print((i.split()))
+    #print(i[i.find(":") + 1:])
+    #print(i[i.find(":") + 1:], end = "")
     #print(i[i.find(":") + 1:].strip())
 #print(studs.readlines())
 
