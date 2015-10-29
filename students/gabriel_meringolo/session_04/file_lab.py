65adeab22a8a8ef42a2d090798d1019f2ed2fae1
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-10-29 12:35:50 -0700

    formating

--- students/gabriel_meringolo/session_04/file_lab.py
+++ students/gabriel_meringolo/session_04/file_lab.py
@@ -1,10 +1,16 @@
-studs = open("students.txt", "r+")
-studs.readline()
+studs = open("students.txt", "r+") # opens files and creates file object
+studs.readline() # reads first line of file "skipping" name/ languages
+lan_str = ""
 for i in studs:
-    cleani = i.strip()
+    lan_str += i.strip()[i.find(":") + 1:]
+print(lan_str.replace(" ","\n").replace(",","")) # replaces spaces with \n and removes ,
+
+    #print(type(i.strip()[i.find(":") + 1:]), end="")
+    #print(i[i.find(":") + 1:], end = "") # prints languages
+    #cleani = i.strip()
     #print(cleani)
-    if cleani:
-        print(cleani[cleani.find(":") + 1:], end = "")
+    #if cleani:
+    #    print(cleani[cleani.find(":") + 1:].strip(","), end = "")
     #print(i[i.find(":") + 1:].strip("\n"))
     #print((i.split()))
     #print(i[i.find(":") + 1:])
