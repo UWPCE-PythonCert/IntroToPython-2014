0fb677a8c8c2d7bd460ad32dbf153283cbad5c19
Ian Cote <iancote@uw.edu>
2015-10-30 01:03:37 -0700

    trigram.py writes stories now.  Hoo, boy.

--- students/iancote/session04/trigram.py
+++ students/iancote/session04/trigram.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 
 def generate_trigram_db(filename):
@@ -25,3 +26,33 @@ def generate_trigram_db(filename):
                     db["{} {}".format(l[i], l[i + 1])] = [l[i + 2]]
             l = l[-2:]
     return db
+
+
+def write_story(db, length):
+    ''' Return string containing story created from db and begin of length '''
+    story = list(random.choice(begin_story(db)).split(' '))
+
+    cursor = 0
+
+    while cursor < (length - 2):
+        # Match on last two words to get db.key
+        # choose random option from list
+        word_list = db["{} {}".format(story[cursor], story[cursor + 1])]
+        story.append(random.choice(word_list))
+        cursor += 1
+
+    return story
+
+
+def begin_story(db):
+    ''' Return list of all dict keys that start with a capital letter. '''
+    l = []
+
+    for i in db.keys():
+        x = []
+        x += i
+        if x[0].isupper():
+            l.append(i)
+    return l
+
+
