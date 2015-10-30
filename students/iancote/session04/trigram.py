2a60018a5df42cb43f68cb21f5bd9007e1d2aa73
Ian Cote <ian@gonesailing.org>
2015-10-29 17:00:10 -0700

    main import function is working!

--- students/iancote/session04/trigram.py
+++ students/iancote/session04/trigram.py
@@ -1,11 +1,27 @@
+import re
+
+
 def generate_trigram_db(filename):
     ''' Returns trigram dict
-        f is a file descriptor '''
+        f is a valid file name of a text Project Gutenberg ebook
+        Punctuation is treated as a word
+    '''
+
     db = {}
+    l = []
+
     with open(filename, 'r') as f:
         for line in f.readlines():
-            array = line.split(' ')
-            for i in range(len(array) - 2):
-                if array[i + 2] not in db["{} {}".format(array[i],
-                   array[i + 1])]:
-                    db["{} {}".format(array[i], array[i + 1])] = array[i + 2]
+            if line.startswith('*** START OF THIS PROJECT GUTENBERG'):
+                continue
+            if line.startswith('*** END OF THIS PROJECT GUTENBERG'):
+                break
+            l += re.findall(r"[\w']+|[.,!?;]", line)
+            for i in range(len(l) - 2):
+                try:
+                    if l[i + 2] not in db["{} {}".format(l[i], l[i + 1])]:
+                        db["{} {}".format(l[i], l[i + 1])].append(l[i + 2])
+                except KeyError:
+                    db["{} {}".format(l[i], l[i + 1])] = [l[i + 2]]
+            l = l[-2:]
+    return db
