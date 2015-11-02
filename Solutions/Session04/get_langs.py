abc98f6af4d95e8e2c3c0828edb5204a828130ce
Chris Barker <PythonCHB@gmail.com>
2015-11-01 23:02:03 -0800

    updated get_langs for python 3 and added Counter solution

--- Solutions/Session04/get_langs.py
+++ Solutions/Session04/get_langs.py
@@ -1,23 +1,48 @@
 #!/usr/bin/env python
 
 """
-script to determine what programming languages students came to this clas iwth
+script to determine what programming languages students came to this clas with
 """
 
-file_path = '../../Examples/Session01/students.txt'
+infilename = "students.txt"
 
-all_langs = set() # use a set to ensure unique values
+all_langs = set()  # use a set to ensure unique values
 
-f = open(file_path) # default read text mode
+f = open(infilename)  # default read text mode
 
-f.readline() # read and toss the header
+f.readline()  # read and toss the header
 
 for line in f:
-    langs = line.split(':')[1]
-    langs = langs.split(',')
+    langs = line.split(':')[1]  # toss the names
+    # a bit of cleanup:
+    langs = langs.replace(',', ' ')
+    langs = langs.replace('and', ' ')
+    langs = langs.split()  # separate the languages
     for lang in langs:
-        lang = lang.strip().lower()
-        if lang: # don't want empty strings
+        lang = lang.strip().capitalize()  # clean them up -- and make case the same
+        if lang:  # don't want empty strings
             all_langs.add(lang)
 for lang in all_langs:
-    print lang
+    print(lang)
+
+# extra credit version: using collections.Counter:
+from collections import Counter
+
+all_langs = Counter()
+
+f = open(infilename)  # default read text mode
+f.readline()  # read and toss the header
+
+for line in f:
+    langs = line.split(':')[1]  # toss the names
+    # a bit of cleanup:
+    langs = langs.replace(',', ' ').replace('and', ' ')
+    langs = langs.split()  # separate the languages
+    for lang in langs:
+        lang = lang.strip().capitalize()  # clean them up -- and make case the same
+        if lang:  # don't want empty strings
+            all_langs[lang] += 1
+
+print("And now the counted version")
+for lang, count in all_langs.items():
+    print(lang, ":", count)
