b44d20c495bcc2582b91a257eb2de957bef1e2cd
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-11-02 23:32:06 -0800

    cleaned up

--- students/gabriel_meringolo/session_04/trigram.py
+++ students/gabriel_meringolo/session_04/trigram.py
@@ -8,28 +8,9 @@ for i in range(len(listr) - 2):
     pair = str(listr[i:i+2])
     follower = listr[i+2]
     if pair in trigram_dict:
-        #print("found")
         trigram_dict[pair] = trigram_dict.get(pair) + follower + " "
-        #continue
     else:
         trigram_dict[pair] = follower + " "
-    #for i in trigram_dict:
-        #print(i)
-        #print(pair)
-        #if i == pair:
-            #trigram_dict[i] = follower
 
-
-
-
-
-    #trigram_dict.append(list((tuple(pair),follower)))
-
-#for i in trigram_dict:
-#    print(trigram_dict.get(i))
 for keys, values in trigram_dict.items():
     print(keys, " => ", values)
-#print(trigram_dict.items())
-#for i in trigram_dict:
-    #print(i[0])
-    #print(" ".join(i[0]), " => ", i[1:])
