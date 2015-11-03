1a3220cbaf7945ffb2e5eba3fd5fd90fb1fea15b
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-11-02 23:31:37 -0800

    changed list to dictionary, much better

--- students/gabriel_meringolo/session_04/trigram.py
+++ students/gabriel_meringolo/session_04/trigram.py
@@ -2,19 +2,34 @@ string = open("sherlock.txt", "r").read()
 listr = string[1151:3956].replace(",","").replace(".","").split()
 
 
-trigram_list = []
+trigram_dict = {}
 pair = ()
 for i in range(len(listr) - 2):
-    pair = listr[i:i+2]
+    pair = str(listr[i:i+2])
     follower = listr[i+2]
-    for i in trigram_list:
-        if i[0] == tuple(pair):
-            i.append(follower)
+    if pair in trigram_dict:
+        #print("found")
+        trigram_dict[pair] = trigram_dict.get(pair) + follower + " "
+        #continue
+    else:
+        trigram_dict[pair] = follower + " "
+    #for i in trigram_dict:
+        #print(i)
+        #print(pair)
+        #if i == pair:
+            #trigram_dict[i] = follower
 
 
 
-    trigram_list.append(list((tuple(pair),follower)))
 
 
-for i in trigram_list:
-    print(" ".join(i[0]), " => ", i[1:])
+    #trigram_dict.append(list((tuple(pair),follower)))
+
+#for i in trigram_dict:
+#    print(trigram_dict.get(i))
+for keys, values in trigram_dict.items():
+    print(keys, " => ", values)
+#print(trigram_dict.items())
+#for i in trigram_dict:
+    #print(i[0])
+    #print(" ".join(i[0]), " => ", i[1:])
