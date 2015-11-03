83f2111447a64f6c6d1e78717deccb7f5c91d76f
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-11-03 12:28:20 -0800

    simple working copy, need to implement saftey features

--- students/gabriel_meringolo/session_04/file_copy.py
+++ students/gabriel_meringolo/session_04/file_copy.py
@@ -6,23 +6,14 @@
 
 #Note that if you want it to do any kind of file,
 # you need to open the files in binary mode: open(filename, 'rb') (or 'wb' for writing.)
-import os
+
 
 def copy_file(original_file, new_file):
     file_original = open(original_file) # creates file object from txt file
-    if new_file in os.listdir(os.getcwd()):
-        over = input("File already exists, overwrite file? Y/N ")
-        if over.lower() == 'y' or over.lower() == "yes":
-            file_copy = open(new_file, "w") # creates empty new file
-            file_copy.write(file_original.read()) # reads the file object and 'writes' it to new file
-            file_original.close() # closes old file
-            file_copy.close() # closes new file
-        if over.lower() == 'n' or over.lower() == "no":
-            new_name = input("Please enter new file name\n> ")
-            file_copy = open(new_name, "w") # creates empty new file
-            file_copy.write(file_original.read()) # reads the file object and 'writes' it to new file
-            file_original.close() # closes old file
-            file_copy.close() # closes new file
+    file_copy = open(new_file, "w") # creates empty new file
+    file_copy.write(file_original.read()) # reads the file object and 'writes' it to new file
+    file_original.close() # closes old file
+    file_copy.close() # closes new file
     print("Copied contents of {} to {}".format(original_file, new_file))
 
 copy_file("sherlock.txt","copy.txt")
