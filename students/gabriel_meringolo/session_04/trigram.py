e11a137189af7724a40233f7bd7d974ac306af97
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-11-03 00:33:09 -0800

    more formating

--- students/gabriel_meringolo/session_04/trigram.py
+++ students/gabriel_meringolo/session_04/trigram.py
@@ -1,16 +1,26 @@
-string = open("sherlock.txt", "r").read()
-listr = string[1151:3956].replace(",","").replace(".","").split()
+import random
+
+string = open("sherlock.txt", "r")
+for i in range(random.randint(20,1000)):
+    string.readline()
+
+listr = string.read().replace(",","").replace('"',"").replace(".","").split()
 
 
 trigram_dict = {}
 pair = ()
+
 for i in range(len(listr) - 2):
-    pair = str(listr[i:i+2])
-    follower = listr[i+2]
-    if pair in trigram_dict:
-        trigram_dict[pair] = trigram_dict.get(pair) + follower + " "
-    else:
-        trigram_dict[pair] = follower + " "
+     pair = str(listr[i:i+2])
+     follower = listr[i+2]
+     if pair in trigram_dict:
+         trigram_dict[pair] = trigram_dict.get(pair) + follower + " "
+     if pair not in trigram_dict:
+         trigram_dict[pair] = follower + " "
+     if len(trigram_dict) == 101:
+         break
+
+
 
 for keys, values in trigram_dict.items():
     print(keys.replace("'","").replace("[","").replace("]","").replace(",",""), " => ", values)
