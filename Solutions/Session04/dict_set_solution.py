095b8bb1d12358b1b3fbd2a56c85c926b39b5046
Chris Barker <PythonCHB@gmail.com>
2015-11-02 21:08:21 -0800

    Added Solutions for the session 4 exercises

---
+++ Solutions/Session04/dict_set_solution.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+"""
+dict/set lab solutions: Chris' version.
+"""
+
+# Create a dictionary containing "name", "city", and "cake" for
+# "Chris" from "Seattle" who likes "Chocolate".
+
+
+d = {"name": "Chris",
+     "city": "Seattle",
+     "cake": "Chocolate"}
+
+# Display the dictionary.
+print(d)
+
+# or something fancier, like:
+print ("{name} is from {city}, and likes {cake} cake.".format(name=d['name'],
+                                                              city=d['city'],
+                                                              cake=d['cake']))
+
+# But if that seems like unnecceasry typing -- it is:
+# we'll learn the **d form is session05:
+print("{name} is from {city}, and likes {cake} cake.".format(**d))
+
+
+# Delete the entry for "cake".
+# Display the dictionary.
+
+del d["cake"]
+
+print(d)
+
+# Add an entry for "fruit" with "Mango" and display the dictionary.
+
+d['fruit'] = 'Mango'
+
+print(d)
+
+# Display the dictionary keys.
+
+print(d.keys())
+
+# Display the dictionary values.
+
+print(d.values())
+
+# Display whether or not "cake" is a key in the dictionary (i.e. False) (now).
+
+print('cake' in d)
+
+# Display whether or not "Mango" is a value in the dictionary.
+
+print('Mango' in d.values())
+
+# Using the dict constructor and zip, build a dictionary of numbers
+# from zero to fifteen and the hexadecimal equivalent (string is fine).
+# did you find the hex() function?"
+nums = range(16)
+hexes = []
+for num in nums:
+    hexes.append(hex(num))
+
+hex_dict = dict(zip(nums, hexes))
+
+print(hex_dict)
+
+
+# Using the dictionary from item 1: Make a dictionary using the same keys
+# but with the number of 't's in each value.
+
+a_dict = {}
+for key, val in d.items():
+    a_dict[key] = val.count('t')
+print(a_dict)
+
+
+# replacing the values in the original dict:
+for key, val in d.items():
+    d[key] = val.count('t')
+print(d)
+
+# Or the direct way -- update() is very handy!
+d.update(a_dict)
+print(d)
+
+# Create sets s2, s3 and s4 that contain numbers from zero through
+# twenty, divisible 2, 3 and 4.
+
+# Display the sets.
+
+s2 = set()
+s3 = set()
+s4 = set()
+for i in range(21):
+    if not i % 2:
+        s2.add(i)
+    if not i % 3:
+        s3.add(i)
+    if not i % 4:
+        s4.add(i)
+
+print(s2)
+print(s3)
+print(s4)
+
+
+# Display if s3 is a subset of s2 (False)
+print(s3.issubset(s2))
+
+# and if s4 is a subset of s2 (True).
+print(s4.issubset(s2))
+
+# Create a set with the letters in 'Python' and add 'i' to the set.
+
+s = set('Python')
+s.add('i')
+
+print(s)
+
+# maybe:
+s = set('Python'.lower())  # that wasn't specified...
+s.add('i')
+
+# Create a frozenset with the letters in 'marathon'
+
+fs = frozenset('marathon')
+
+# display the union and intersection of the two sets.
+
+print("union:", s.union(fs))
+print("intersection:", s.intersection(fs))
+
+# note that order doesn't matter for these:
+
+print("union:", fs.union(s))
+print("intersection:", fs.intersection(s))
---
+++ Solutions/Session04/trigram.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+"""
+Trigram.py
+
+A solution to the trigram coding Kata:
+
+http://codekata.com/kata/kata14-tom-swift-under-the-milkwood/
+
+Chris Barker's Solution
+
+This one is pretty straight forward -- really a quickie script
+
+There is lots of room to make it fancier of you want
+"""
+
+# infilename = "sherlock_small.txt"
+infilename = "sherlock.txt"
+
+import sys
+import string
+import random
+
+
+def strip_punctuation(text):
+    """
+    strips the punctuation from a bunch of text
+    """
+    # build a translation table for string.translate:
+    # there are other ways to do this:
+
+    # create a translation table to replace all punctuation with spaces
+    #    -- then split() will remove the extra spaces
+    punctuation = string.punctuation
+    punctuation = punctuation.replace("'", "")  # keep apostropies
+    punctuation = punctuation.replace("-", "")  # keep hyphenated words
+    # building a translation table
+    table = {}
+    for c in punctuation:
+        table[ord(c)] = ' '
+    # remove punctuation with the translation table
+    text = text.translate(table)
+    # remove "--" -- can't do multiple characters with translate
+    text = text.replace("--", " ")
+
+    return text
+
+
+def make_words(text):
+
+    """
+    make a list of words from a large bunch of text
+
+    strips all the punctuation and other stuff from a string
+    """
+    text = strip_punctuation(text)
+
+    # lower-case everything to remove that complication:
+    text = text.lower()
+
+    # split into words
+    words = text.split()
+
+    # remove the bare single quotes: "'" is both a quote and an apostrophe
+    # and capitalize "i"
+    words2 = []
+    for word in words:
+        if word != "'":  # remove quote by itself
+            # "i" by itself should be capitalized
+            words2.append("I" if word == 'i' else word)
+    # could be done with list comp too -- next week!
+    # words2 = [("I" if word == 'i' else word) for word in words if word != "'"]
+    return words2
+
+
+def read_in_data(infilename):
+
+    infile = open(infilename, 'r')  # text mode is default
+    # strip out the header, table of contents, etc.
+    for i in range(61):
+        infile.readline()
+
+    full_text = []
+    # read the rest of the file line by line
+    for line in infile:
+        if line.startswith("End of the Project Gutenberg EBook"):
+            break
+        full_text.append(line)
+
+    # put all the lines together into one big string:
+    return " ".join(full_text)
+
+
+def build_trigram(words):
+    """build a trigram dict from the passed-in list of words"""
+
+    # Dictionary for trigram results:
+    # The keys will be all the word pairs
+    # The values will be a list of the words that follow each pair
+    word_pairs = {}
+
+    # loop through the words
+    # (rare case where using the index to loop is easiest)
+    for i in range(len(words) - 2):  # minus 2, 'cause you need a pair'
+        pair = tuple(words[i:i+2])  # a tuple so it can be a key in the dict
+        follower = words[i+2]
+        word_pairs.setdefault(pair, []).append(follower)
+
+        # setdefault() returns the value if pair is already in the dict
+        #    if it's not, it adds it, setting the value to a an empty list
+        #    then it returns the list, which we then append the following
+        #    word to -- cleaner than:
+        # if pair in word_pairs:
+        #     word_pairs[pair].append(follower)
+        # else:
+        #     word_pairs[pair] = [follower]
+    return word_pairs
+
+
+def build_text(word_pairs):
+
+    """
+    Build some new text from the word_pair dict supplied
+
+    A bit of fancy stuff to make them look like sentences..
+    """
+
+    new_text = []
+    for i in range(30):  # do thirty sentences
+        # pick a word pair to start the sentence
+        # need to make dict.keys() a list to randomly select from it
+        sentence = list(random.choice(list(word_pairs.keys())))
+        # now add a random number of additional words to the sentence
+        for j in range(random.randint(2, 10)):
+            pair = tuple(sentence[-2:])
+            sentence.append(random.choice(word_pairs[pair]))
+
+        # capitalize the first word:
+        sentence[0] = sentence[0].capitalize()
+
+        # Add the period
+        sentence[-1] += "."
+        new_text.extend(sentence)
+
+    new_text = " ".join(new_text)
+
+    return new_text
+
+
+if __name__ == "__main__":
+    # get the filename from the command line
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print("You must pass in a filename")
+        sys.exit(1)
+
+    in_data = read_in_data(filename)
+    words = make_words(in_data)
+    word_pairs = build_trigram(words)
+    new_text = build_text(word_pairs)
+
+    print(new_text)
