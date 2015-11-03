48cd21131eb88734b59d90a2be9517a9971554f5
Erica Winberry <elaewin@gmail.com>
2015-11-03 15:11:47 -0500

    Changed read_student_data to use a with statement to open the file, rather than explicitly opening and closing the file.

--- students/erica_winberry/session04/file_lab.py
+++ students/erica_winberry/session04/file_lab.py
@@ -4,24 +4,21 @@ os.chdir("../../../Examples")
 
 def read_student_data(filename):
     student_data = []
-    f = open(filename, "r")
-    for line in f:
-        line = line.strip()
-        line = line.split(":")
-        student, langs = line
-        langs = langs.replace(",", "")
-        langs = langs.replace("and", "")
-        langs = langs.split(" ")
-        student_data.append(langs)
-    f.close()
-    
-    for item in student_data:
-        for t in item:
-            if t == "":
-                item.remove(t)
-    
-    return student_data
-    
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            line = line.split(":")
+            student, langs = line
+            langs = langs.replace(",", "")
+            langs = langs.replace("and", "")
+            langs = langs.split(" ")
+            student_data.append(langs)       
+        for item in student_data:
+            for t in item:
+                if t == "":
+                    item.remove(t)     
+        return student_data
+
 
 def count_langs(l):
     languages = {}
