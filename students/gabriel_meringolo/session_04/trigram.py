e2d8c870e1a49297654524ef90c7b3257f90b19a
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-11-03 22:52:48 -0800

    final copy, works'

--- students/gabriel_meringolo/session_04/trigram.py
+++ students/gabriel_meringolo/session_04/trigram.py
@@ -1,37 +1,28 @@
 import random
+import textwrap
 
 string = open("sherlock.txt", "r").read()
-#for i in range(random.randint(20, 1000)):
-#    string.readline()
 
-listr = string[1151:-2938].replace('"', "").split()
+listr = string[1151:-2938].replace('"', "").replace("(", "").replace(")", "").split()
 
 trigram_dict = {}
-pair = ()
 
 for i in range(len(listr) - 2):
-    pair = str(listr[i:i+2]) #str(listr[i:i+2])
-    #print(pair)
-    #print(type(pair))
-    #raise Exception
+    pair = tuple(listr[i:i+2]) #str(listr[i:i+2])
     follower = listr[i+2]
     if pair in trigram_dict:
         trigram_dict[pair] = trigram_dict.get(pair) + follower + " "
-    if pair not in trigram_dict:
+    else:
         trigram_dict[pair] = follower + " "
-    #if len(trigram_dict) == 200:
-    #    break
-
-#for keys, values in trigram_dict.items():
-#    print(keys.replace("'", "").replace("[", "").replace("]", "").replace(",", ""), " => ", values)
-
 
 new_story = []
 start_pair = random.choice(list(trigram_dict.keys())) # get random key pair and start a new story..
 
-print(start_pair)
-#print((" ".join(start_pair.split())))
-#new_story.append(start_pair.split())
-#print(new_story)
-#for i in range(250):
+new_story.extend(start_pair)
+
+for i in range(300):
+    new_story.append(random.choice(trigram_dict.get(start_pair).split()))
+    start_pair = tuple(new_story[-2:])
+new_book = " ".join(new_story).capitalize()
+print(textwrap.fill(new_book, 70), end= ".")
 
