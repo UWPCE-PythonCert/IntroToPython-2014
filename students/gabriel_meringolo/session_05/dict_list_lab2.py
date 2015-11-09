5d6ee5aab4f3afe2b73889725e8eea32fb8921e1
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-11-08 19:27:53 -0800

    more work, more confusion

--- students/gabriel_meringolo/session_05/dict_list_lab2.py
+++ students/gabriel_meringolo/session_05/dict_list_lab2.py
@@ -1,8 +1,8 @@
-#food_prefs = {"name": "Chris", "city": "Seattle", "cake": "chocolate",
-#              "fruit": "mango", "salad": "caesar", "pasta": "lasagna"}
+food_prefs = {"name": "Chris", "city": "Seattle", "cake": "chocolate",
+              "fruit": "mango", "salad": "caesar", "pasta": "lasagna"}
 
-#print("{name} is from {city} he likes {cake} cake,\n{fruit} smoothies, extra dressing \
-#on his {salad}\nsalads and always asks for seconds of {pasta}.".format(**food_prefs))
+print("{name} is from {city} he likes {cake} cake,\n{fruit} smoothies, extra dressing \
+on his {salad}\nsalads and always asks for seconds of {pasta}.".format(**food_prefs))
 
 
 #Using a list comprehension, build a dictionary of numbers from zero to fifteen and the hexadecimal
