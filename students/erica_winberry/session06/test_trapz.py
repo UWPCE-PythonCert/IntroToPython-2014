4ce26f0919e3f621a5667b2f137912c64a3de079
Erica Winberry <elaewin@gmail.com>
2015-11-12 09:25:47 -0800

    Started working on the trapz lab for week 6: created trapezoidal.py and the test file test_trapz.py.

---
+++ students/erica_winberry/session06/test_trapz.py
@@ -0,0 +1,13 @@
+
+from trapezoidal import trapz
+
+'''
+About the simplest “curve” you can have is a horizontal straight line, 
+in this case, at y = 5. The area under that line from 0 to 10 is a 
+rectangle that is 10 wide and 5 high, so with an area of 50.
+
+Of course in this case, it’s easiest to simply multiply the height 
+times the width, but we want a function that will work for Any curve.
+
+HINT: this simple example could be a good test case!
+'''
