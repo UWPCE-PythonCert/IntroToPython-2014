043a9a97236f7b8b436f687c52ab9d2ddc76f399
Eric Vegors <tycobass@gmail.com>
2015-11-13 21:15:51 -0800

    Added Session_06

---
+++ students/eric_v/Session_06/lambda_keyword.py
@@ -0,0 +1,5 @@
+def function_builder(i):
+    list1 = []
+    for j in range(i):
+        list1.append(j)
+    return list1
---
+++ students/eric_v/Session_06/lamda_keyword.py
@@ -0,0 +1,5 @@
+def function_builder(i):
+    l1 = []
+    for j in range(i):
+        l1.append(j)
+    return l1
