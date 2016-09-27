4296ee067e3963ed59c0aa4e66cdd2f3f751241c
Chris Barker <PythonCHB@gmail.com>
2016-09-26 22:59:23 -0700

    updated scheduling program

--- Examples/Session01/schedule.py
+++ Examples/Session01/schedule.py
@@ -8,16 +8,17 @@ students = open('students.txt').readlines()
 # remove the header line
 del students[0]
 
+# strip the whitespace
+students = [line.strip() for line in students]
+
 # remove the languages, colon, etc.
 students = [line.split(":")[0] for line in students]
 
 # reverse the first, last names
-
 # separate them:
 students = [line.split(",") for line in students]
-
 # put them back together
-students = ["{} {}".format(first, last) for last, first in students]
+students = ["{} {}".format(first.strip(), last) for last, first in students]
 
 # put them in random order
 random.shuffle(students)
