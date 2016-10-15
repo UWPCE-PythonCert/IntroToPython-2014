43b64cd3fe102e5e92d2df4b62b18fc9a7fc33a9
Chris Barker <PythonCHB@gmail.com>
2016-10-15 14:28:10 -0700

    added an example of __main__

---
+++ Examples/Session04/__main__example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+print("every module has a __name__")
+
+print("What it is depends on how it is used")
+
+print("right now, this module's __name is: {}".format(__name__))
+
+# so if you want coce to run only when a module is a top level script,
+# you use this clause:
+if __name__ == "__main__":
+    print("I must be running as a top-level script")
